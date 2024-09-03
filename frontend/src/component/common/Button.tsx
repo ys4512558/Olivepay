ad6e323ca8e493166af4f1b6b7bdaca048cba02b
iltae94 <iltae94@gmail.com>
2024-09-03 16:05:40 +0900

    :wrench: Config: 전역 저장소 zotai 설정 + 폴더 단위 집중화 관리
    S11P21A601-158

--- frontend/src/component/common/Button.tsx
+++ frontend/src/component/common/Button.tsx
@@ -10,8 +10,10 @@ const BUTTON_VARIANTS = {
   danger: tw`bg-red-500 text-white hover:bg-red-600`,
 };
 
-export const Button = () => {
+const Button = () => {
   return (
     <button className={clsx(BUTTON_VARIANTS.primary, 'w-1/2')}>되나</button>
   );
 };
+
+export default Button;
--- frontend/src/pages/Main.tsx
+++ frontend/src/pages/Main.tsx
@@ -1,11 +1,18 @@
-import { Button } from '../component/common/Button';
+import { useAtom } from 'jotai';
+import { userAtom } from '../atoms';
+
+import { Button } from '../component/common';
 
 const Main = () => {
+  const [user, setUser] = useAtom(userAtom);
   return (
     <>
       <p className="m-2 w-1/2 bg-red-200 p-4 py-2 text-center text-4xl">
-        설정 완료!
+        설정 완료! by {user.name}
       </p>
+      <button onClick={() => setUser({ name: '소희', isLoggedIn: true })}>
+        바꿔!
+      </button>
       <Button />
     </>
   );
