57fbfad4f60008839fd5ff492d4ebd9a707afa99
iltae94 <iltae94@gmail.com>
2024-09-04 15:36:20 +0900

    :recycle: Refactor: 뒤로가기 버튼
    S11P21A601-171

--- frontend/src/component/common/BackButton.tsx
+++ frontend/src/component/common/BackButton.tsx
@@ -1,12 +1,11 @@
+import { useNavigate } from 'react-router-dom';
 import { ChevronLeftIcon } from '@heroicons/react/24/solid';
 
 const BackButton = () => {
+  const navigate = useNavigate();
   return (
-    <button
-      className="rounded border-2 p-2"
-      onClick={() => window.history.back()}
-    >
-      <ChevronLeftIcon className="h-5 w-5" />
+    <button className="rounded border-2 p-2" onClick={() => navigate(-1)}>
+      <ChevronLeftIcon className="size-5" />
     </button>
   );
 };
