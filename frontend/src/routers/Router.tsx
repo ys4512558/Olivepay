8b5582c03d412aaa6003ddd53f3c8731d2158013
iltae94 <iltae94@gmail.com>
2024-09-06 14:08:33 +0900

    :sparkles: Feat: 404 페이지 및 라우팅 수정
    S11P21A601-182

--- frontend/src/routers/Router.tsx
+++ frontend/src/routers/Router.tsx
@@ -1,18 +1,18 @@
-import { Route, Routes, Navigate } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 import { Suspense, lazy } from 'react';
 
-import { LoadingPage } from '../pages';
-
-const Main = lazy(() => import('../pages/MainPage'));
-const Signup = lazy(() => import('../pages/SignupPage'));
+const MainPage = lazy(() => import('../pages/MainPage'));
+const SignupPage = lazy(() => import('../pages/SignupPage'));
+const LoadingPage = lazy(() => import('../pages/LoadingPage'));
+const NotFoundPage = lazy(() => import('../pages/NotFoundPage'));
 
 const Router = () => {
   return (
     <Suspense fallback={<LoadingPage />}>
       <Routes>
-        <Route path="/" element={<Main />} />
-        <Route path="/signup" element={<Signup />} />
-        <Route path="*" element={<Navigate to="/" replace />} />
+        <Route path="/" element={<MainPage />} />
+        <Route path="/signup" element={<SignupPage />} />
+        <Route path="*" element={<NotFoundPage />} />
       </Routes>
     </Suspense>
   );
