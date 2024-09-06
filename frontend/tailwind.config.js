c345f5f6e743a1650e848b5a2b86d06ea8ff2c37
dlthgml1501 <dlthgml1501@gmail.com>
2024-09-06 18:02:07 +0900

    :lipstick: Style: 카드 배경색 추가

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -2,7 +2,11 @@
 export default {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        꿈나: 'linear-gradient(45deg, #229b41, #32c93c, #a5ce14)',
+      },
+    },
   },
   plugins: [],
 };
