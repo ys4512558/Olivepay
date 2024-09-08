d755551f79e3990104fae7fb3a7f05d1ca644f45
iltae94 <iltae94@gmail.com>
2024-09-08 17:09:27 +0900

    :sparkles: Feat: 별점 컴퍼넌트
    S11P21A601-183

---
+++ frontend/src/component/common/StarRating.tsx
@@ -0,0 +1,21 @@
+import ReactStars from 'react-rating-stars-component';
+import { StarIcon } from '@heroicons/react/24/solid';
+
+const StarRating: React.FC<StarRatingProps> = ({
+  value,
+  canEdit = false,
+  onChange,
+}) => {
+  return (
+    <ReactStars
+      count={5}
+      value={value}
+      edit={canEdit}
+      onChange={onChange}
+      emptyIcon={<StarIcon className="size-5 text-BASE" />}
+      filledIcon={<StarIcon className="size-5 text-YELLOW" />}
+    />
+  );
+};
+
+export default StarRating;
