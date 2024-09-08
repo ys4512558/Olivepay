1be643c4b1718c4151e2f38657b43b46a238be3c
242lee <iamurmaster_@naver.com>
2024-09-08 17:25:49 +0900

    :sparkles: Feat: 카드, 쿠폰, 제목 컴포넌트 추가

--- frontend/src/component/common/index.ts
+++ frontend/src/component/common/index.ts
@@ -1,1 +1,4 @@
 export { default as Button } from './Button';
+export { default as CreditCard } from './creditCard/CreditCard';
+export { default as Coupon } from './Coupon'
+export { default as PageTitle } from './PageTitle'
