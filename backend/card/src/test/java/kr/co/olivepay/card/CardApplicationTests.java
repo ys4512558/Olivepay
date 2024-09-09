bf1430b7a7bd96d40e777647e9903550194df795
ys4512558 <ys4512558@naver.com>
2024-09-09 14:23:38 +0900

    :tada: Init: 프로젝트 생성

---
+++ backend/card/src/test/java/kr/co/olivepay/card/CardApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.co.olivepay.card;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
