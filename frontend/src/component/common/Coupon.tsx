5462e3c7b810137c61d21507280342995ac70b5c
dlthgml1501 <dlthgml1501@gmail.com>
2024-09-09 11:14:56 +0900

    :recycle: Refrator: 시멘틱 태그 수정

--- frontend/src/component/common/Coupon.tsx
+++ frontend/src/component/common/Coupon.tsx
@@ -1,30 +1,34 @@
-const Coupon: React.FC<CouponProps> = ({ storeName, cost, couponID, onClick }) => {
-
+const Coupon: React.FC<CouponProps> = ({
+  storeName,
+  cost,
+  couponID,
+  onClick,
+}) => {
   return (
-    <div className="relative w-80 h-48 flex flex-col justify-center items-center">
+    <main className="relative flex h-48 w-80 flex-col items-center justify-center">
       {/* 쿠폰 */}
-      <div className="relative w-full h-full bg-orange-500 flex flex-col justify-center p-6 shadow-lg">
-        <div className="items-start justify-center flex-grow">
-          <div className="text-white text-sm">{couponID}</div>
-          <div className="text-white text-sm">{storeName}</div>
-          <div className="text-white text-4xl font-bold mt-2">{cost}원</div>
-        </div>
+      <section className="relative flex h-full w-full flex-col justify-center p-6 shadow-lg">
+        <article className="flex-grow items-start justify-center">
+          <div className="text-sm text-white">{couponID}</div>
+          <div className="text-sm text-white">{storeName}</div>
+          <div className="mt-2 text-4xl font-bold text-white">{cost}원</div>
+        </article>
         <div className="text-xs text-white">발급일 23:59까지 사용 가능</div>
 
         {/* 다운로드 버튼 */}
         <button
           onClick={onClick}
-          className="absolute -bottom-2 -right-4 w-24 h-14 bg-gray-300 rounded-full shadow-lg flex justify-center items-center"
+          className="absolute -bottom-2 -right-4 flex h-14 w-24 items-center justify-center rounded-full bg-gray-300 shadow-lg"
         >
           {/* 구름 윗부분 */}
-          <div className="absolute -top-4 w-12 h-11 bg-gray-300 rounded-full"></div>
+          <div className="absolute -top-4 h-11 w-12 rounded-full bg-gray-300"></div>
           {/* 아래 방향 화살표 */}
-          <div className="relative w-6 h-6 bg-transparent">
-            <div className="absolute left-0 right-0 mx-auto w-4 h-4 border-b-4 border-r-4 border-white transform rotate-45"></div>
+          <div className="relative h-6 w-6 bg-transparent">
+            <div className="absolute left-0 right-0 mx-auto h-4 w-4 rotate-45 transform border-b-4 border-r-4 border-white"></div>
           </div>
         </button>
-      </div>
-    </div>
+      </section>
+    </main>
   );
 };
 
