dc0bc9dd89361b22a153f514379a66c49a98d661
dlthgml1501 <dlthgml1501@gmail.com>
2024-09-09 11:07:11 +0900

    :sparkles: Feat: 카드 배경색 추가

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -4,9 +4,14 @@ export default {
   theme: {
     extend: {
       backgroundImage: {
-        꿈나: 'linear-gradient(45deg, #229b41, #32c93c, #a5ce14)',
+        DT: 'linear-gradient(45deg, #229b41, #32c93c, #a5ce14)',
+        SH: 'linear-gradient(45deg, #05274b, #324962, #858c94)',
+        WR: 'linear-gradient(45deg, #79b7d0, #3f8fb0, #307b99)',
+        HN: 'linear-gradient(45deg, #1b765f, #1b7476, #307b99)',
+        KM: 'linear-gradient(45deg, #ffb700, #ffe000, #ffb700)',
       },
     },
   },
+  safelist: ['bg-DT', 'bg-SH', 'bg-WR', 'bg-HN', 'bg-KM'],
   plugins: [],
 };
