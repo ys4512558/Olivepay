48f85e3b77efc66f71b62726dd3d3024c3d7aba7
dlthgml1501 <dlthgml1501@gmail.com>
2024-09-09 16:33:54 +0900

    :sparkles: Feat: 버튼 클릭 기능 수정

--- frontend/src/component/common/KeyPad.tsx
+++ frontend/src/component/common/KeyPad.tsx
@@ -1,22 +1,10 @@
-import React from 'react';
 import { BackspaceIcon } from '@heroicons/react/24/solid';
 
-const KeyPad: React.FC<KeyPadProps> = ({ variant, onKeyPress }) => {
-  const handleButtonClick = (value: string | number) => {
-    if (onKeyPress) {
-      onKeyPress(value);
-    }
-    console.log(value);
-  };
-
+const KeyPad: React.FC<KeyPadProps> = ({ variant, onKeyPress = () => {} }) => {
   return (
     <div className="mx-auto grid w-64 grid-cols-3 gap-x-24 gap-y-10 text-center font-bold">
       {[1, 2, 3, 4, 5, 6, 7, 8, 9].map((num) => (
-        <button
-          key={num}
-          onClick={() => handleButtonClick(num)}
-          className="text-2xl"
-        >
+        <button key={num} onClick={() => onKeyPress(num)} className="text-2xl">
           {num}
         </button>
       ))}
@@ -24,28 +12,22 @@ const KeyPad: React.FC<KeyPadProps> = ({ variant, onKeyPress }) => {
       {variant === 'password' ? (
         <>
           <div />
-          <button onClick={() => handleButtonClick(0)} className="text-2xl">
+          <button onClick={() => onKeyPress(0)} className="text-2xl">
             0
           </button>
-          <button
-            onClick={() => handleButtonClick('delete')}
-            className="text-2xl"
-          >
+          <button onClick={() => onKeyPress('delete')} className="text-2xl">
             <BackspaceIcon className="mx-auto h-6 w-6" />
           </button>
         </>
       ) : (
         <>
-          <button onClick={() => handleButtonClick('00')} className="text-2xl">
+          <button onClick={() => onKeyPress('00')} className="text-2xl">
             00
           </button>
-          <button onClick={() => handleButtonClick(0)} className="text-2xl">
+          <button onClick={() => onKeyPress(0)} className="text-2xl">
             0
           </button>
-          <button
-            onClick={() => handleButtonClick('delete')}
-            className="text-2xl"
-          >
+          <button onClick={() => onKeyPress('delete')} className="text-2xl">
             <BackspaceIcon className="mx-auto h-6 w-6" />
           </button>
         </>
