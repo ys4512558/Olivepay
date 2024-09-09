b8e30278cf44722f82cba799f3bca2c5c29679fe
dlthgml1501 <dlthgml1501@gmail.com>
2024-09-09 11:13:42 +0900

    :recycle: Refrator: 공통 컴포넌트 import문 수정

--- frontend/src/component/common/index.ts
+++ frontend/src/component/common/index.ts
@@ -1,4 +1,4 @@
 export { default as Button } from './Button';
 export { default as CreditCard } from './creditCard/CreditCard';
-export { default as Coupon } from './Coupon'
-export { default as PageTitle } from './PageTitle'
+export { default as Coupon } from './Coupon';
+export { default as PageTitle } from './PageTitle';
