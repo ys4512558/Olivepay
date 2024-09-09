1d3710366b854729e60a1ed2e5e9fe2f60d8eb52
dlthgml1501 <dlthgml1501@gmail.com>
2024-09-09 11:15:42 +0900

    :sparkles: Feat: 공통컴포넌트 추가

--- frontend/src/pages/Main.tsx
+++ frontend/src/pages/Main.tsx
@@ -5,9 +5,13 @@ import { userAtom } from '../atoms';
 
 import { getUsersInfo } from '../api/userApi';
 
-import { Button } from '../component/common';
+import { Button, CreditCard, Coupon, PageTitle } from '../component/common';
 
 const Main = () => {
+  const handleDownload = () => {
+    alert('쿠폰을 다운로드합니다!');
+  };
+
   const [users, setUsers] = useAtom(userAtom);
   const { data, error, isLoading } = useQuery({
     queryKey: ['users'],
@@ -24,12 +28,44 @@ const Main = () => {
   if (error) return <div>에러 발생: {error.message}</div>;
   return (
     <>
+      <PageTitle title="test" />
       <div>
         {users.slice(0, 5).map((user) => (
           <div key={user.id}>{user.name}</div>
         ))}
       </div>
       <Button />
+      <CreditCard
+        cardName="꿈나무 8450"
+        cardNumber="0000 1111 2222 3333"
+        cardOwner="김나무"
+      />
+      <CreditCard
+        cardName="신한 6712"
+        cardNumber="110 1111 2222 3333"
+        cardOwner="김신한"
+      />
+      <CreditCard
+        cardName="우리 7821"
+        cardNumber="1002 445 883 123"
+        cardOwner="이우리"
+      />
+      <CreditCard
+        cardName="하나 1423"
+        cardNumber="1002 445 883 123"
+        cardOwner="최하나"
+      />
+      <CreditCard
+        cardName="국민 9015"
+        cardNumber="1002 445 883 123"
+        cardOwner="최하나"
+      />
+      <Coupon
+        couponID={1}
+        storeName="대우부대찌개"
+        cost={2000}
+        onClick={handleDownload}
+      />
     </>
   );
 };
