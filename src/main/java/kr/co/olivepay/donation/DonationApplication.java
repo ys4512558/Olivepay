97cc2904c81f011302be559a161da979478e508b
서정희 <sjhlko@naver.com>
2024-09-09 14:22:16 +0900

    🎉 Init: 프로젝트 생성

---
+++ src/main/java/kr/co/olivepay/donation/DonationApplication.java
@@ -0,0 +1,13 @@
+package kr.co.olivepay.donation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DonationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DonationApplication.class, args);
+    }
+
+}
---
+++ src/test/java/kr/co/olivepay/donation/DonationApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.co.olivepay.donation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DonationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
