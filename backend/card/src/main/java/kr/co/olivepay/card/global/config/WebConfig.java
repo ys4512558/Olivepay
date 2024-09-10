b216c3abe62eed392ae896ec4a18373f1a53d1e9
wnso.kim <wnso.kim@gmail.com>
2024-09-10 15:49:57 +0900

    🔧 Config: Global config 등록
    - #S11P21A601-280

---
+++ backend/card/src/main/java/kr/co/olivepay/card/global/config/WebConfig.java
@@ -0,0 +1,18 @@
+package kr.co.olivepay.card.global.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOriginPatterns("*")
+                .allowedMethods("*")
+                .allowedHeaders("*")
+                .allowCredentials(true)
+                .maxAge(3600);
+    }
+}
