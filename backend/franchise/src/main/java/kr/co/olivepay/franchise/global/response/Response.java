4de50cac05cc329848cbf0e59bce43898b422d1b
wnso.kim <wnso.kim@gmail.com>
2024-09-10 17:33:00 +0900

    🔧 Config: Error Handler 및 Response 등록
    - #S11P21A601-286

---
+++ backend/franchise/src/main/java/kr/co/olivepay/franchise/global/response/Response.java
@@ -0,0 +1,18 @@
+package kr.co.olivepay.franchise.global.response;
+
+import kr.co.olivepay.franchise.global.enums.ErrorCode;
+import org.springframework.http.ResponseEntity;
+
+public record Response<T>(String resultCode, String code, String message, T data) {
+    public static <T> ResponseEntity<Response<T>> success(SuccessResponse<T> response) {
+        return new ResponseEntity<>(new Response<>("SUCCESS", response.successCode().name(),
+                response.successCode().getMessage(), response.data()), response.successCode().getHttpStatus());
+    }
+
+    public static <T> Response<T> error(ErrorCode code, T data) {
+        return new Response<>("ERROR", code.name(), code.getMessage(), data);
+    }
+
+
+}
+
