cce72780ff218b72b65bd59840ed17f52902723f
서정희 <sjhlko@naver.com>
2024-09-11 17:51:02 +0900

    ✨ Feat: 엔티티 작성

---
+++ backend/donation/src/main/java/kr/co/olivepay/donation/entity/Coupon.java
@@ -0,0 +1,35 @@
+package kr.co.olivepay.donation.entity;
+
+import jakarta.persistence.*;
+import kr.co.olivepay.donation.enums.CouponUnit;
+import kr.co.olivepay.donation.global.entity.BaseEntity;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Coupon extends BaseEntity {
+    @Id
+    @Column(name = "coupon_id", nullable = false, columnDefinition = "INT UNSIGNED")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, columnDefinition = "INT UNSIGNED")
+    private Long franchiseId;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "donor_id", nullable = false, columnDefinition = "INT UNSIGNED")
+    private Donation donation;
+
+    @Enumerated(value = EnumType.STRING)
+    private CouponUnit couponUnit;
+
+    @JoinColumn(nullable = false)
+    private String message;
+
+    @JoinColumn(nullable = false, columnDefinition = "INT UNSIGNED")
+    private Long count;
+
+}
---
+++ backend/donation/src/main/java/kr/co/olivepay/donation/enums/CouponUnit.java
@@ -0,0 +1,9 @@
+package kr.co.olivepay.donation.enums;
+
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
+public enum CouponUnit {
+    TWO(2000), FOUR(2000);
+    Integer value;
+}
