5a0037bfb2abd5bba185757268b3011502363f49
dlthgml1501 <dlthgml1501@gmail.com>
2024-09-11 14:07:59 +0900

    :fire: Remove: PageTitle header 삭제

--- frontend/src/component/common/PageTitle.tsx
+++ frontend/src/component/common/PageTitle.tsx
@@ -1,9 +1,5 @@
 const PageTitle: React.FC<PageTitleProps> = ({ title }) => {
-  return (
-    <header className="mt-4 text-center">
-      <h1 className="text-xl font-bold">{title}</h1>
-    </header>
-  );
+  return <h1 className="text-xl font-bold">{title}</h1>;
 };
 
 export default PageTitle;
