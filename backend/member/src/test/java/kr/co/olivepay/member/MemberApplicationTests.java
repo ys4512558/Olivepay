6de1f3ef291a2a2fda9721d459c950a038886413
wnso.kim <wnso.kim@gmail.com>
2024-09-12 16:46:42 +0900

    🎉 Init: 프로젝트 생성
    - #S11P21A601-215

---
+++ backend/member/src/test/java/kr/co/olivepay/member/MemberApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.co.olivepay.member;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MemberApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
