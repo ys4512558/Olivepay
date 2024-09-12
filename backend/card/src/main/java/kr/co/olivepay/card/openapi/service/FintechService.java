207b22faaccdbd81cd3cb7842656a94ddbd38a74
ys4512558@naver.com <ys4512558@naver.com>
2024-09-12 12:40:30 +0900

    :sparkles: Feat: 계좌생성, 카드생성 등 핀테크 API사용 서비스 개발
    - S11P21A601-293: 계좌 생성 서비스 로직 개발
    - S11P21A601-294: 카드 생성 요청 서비스 로직 개발

---
+++ backend/card/src/main/java/kr/co/olivepay/card/openapi/service/FintechService.java
@@ -0,0 +1,24 @@
+package kr.co.olivepay.card.openapi.service;
+
+import kr.co.olivepay.card.openapi.dto.res.account.AccountRec;
+import kr.co.olivepay.card.openapi.dto.res.card.CardRec;
+
+public interface FintechService {
+
+    /**
+     * 계좌 생성
+     *
+     * @param userKey: 유저 키
+     * @return 생성된 계좌 반환 (accountNo)
+     */
+    AccountRec createAccount(String userKey);
+
+    /**
+     * 카드 생성
+     *
+     * @param userKey:             유저 키
+     * @param withdrawalAccountNo: 출금 계좌
+     * @return 생성된 카드 번호 반환 (cardNo)
+     */
+    CardRec createCard(String userKey, String withdrawalAccountNo);
+}
