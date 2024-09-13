7d2456df8b4b988a8210b225cc1fec74eaede106
ys4512558@naver.com <ys4512558@naver.com>
2024-09-13 14:15:56 +0900

    :sparkles: Feat: 카드, 계좌 DTO, Entity간 변환 매퍼 클래스 개발
    - S11P21A601-279

---
+++ backend/card/src/main/java/kr/co/olivepay/card/mapper/CardMapper.java
@@ -0,0 +1,39 @@
+package kr.co.olivepay.card.mapper;
+
+import kr.co.olivepay.card.dto.req.CardRegisterReq;
+import kr.co.olivepay.card.dto.res.MyCardSearchRes;
+import kr.co.olivepay.card.dto.res.TransactionCardSearchRes;
+import kr.co.olivepay.card.entity.Account;
+import kr.co.olivepay.card.entity.Card;
+import kr.co.olivepay.card.entity.CardCompany;
+import kr.co.olivepay.card.openapi.dto.res.card.CardRec;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.Named;
+
+@Mapper(componentModel = "spring")
+public interface CardMapper {
+
+    @Mapping(target = "id", ignore = true)
+    @Mapping(source = "userId", target = "userId")
+    @Mapping(source = "cardRec.cardNo", target = "cardNumber")
+    @Mapping(source = "cardRegisterReq.realCardNumber", target = "realCardNumber")
+    @Mapping(source = "cardRegisterReq.expirationYear", target = "expirationYear")
+    @Mapping(source = "cardRegisterReq.expirationMonth", target = "expirationMonth")
+    @Mapping(source = "cardRec.cvc", target = "cvc")
+    @Mapping(source = "cardRegisterReq.creditPassword", target = "creditPassword")
+    @Mapping(source = "cardRec.cardName", target = "isDefault", qualifiedByName = "mapIsDefault")
+    Card toEntity(Long userId, Account account, CardRec cardRec, CardRegisterReq cardRegisterReq, CardCompany cardCompany);
+
+    @Mapping(source = "card.id", target = "cardId")
+    @Mapping(source = "card.cardCompany.name", target = "cardCompany")
+    MyCardSearchRes toMyCardSearchRes(Card card);
+
+    @Mapping(source = "card.id", target = "cardId")
+    TransactionCardSearchRes toTransactionCardSearchRes(Card card);
+
+    @Named("mapIsDefault")
+    default Boolean mapIsDefault(String cardName) {
+        return cardName.equals("꿈나무카드");
+    }
+}
