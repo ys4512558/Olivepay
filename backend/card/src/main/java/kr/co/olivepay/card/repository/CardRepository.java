be0bdcc5ce4866efee58529f5b9689351629deb5
왕승철 <ys4512558@naver.com>
2024-09-15 17:20:18 +0900

    :sparkles: Feat: 카드 삭제를 위한 쿼리 메서드 개발
    - 카드 ID, 멤버 ID를 통해 카드 삭제

--- backend/card/src/main/java/kr/co/olivepay/card/repository/CardRepository.java
+++ backend/card/src/main/java/kr/co/olivepay/card/repository/CardRepository.java
@@ -11,4 +11,12 @@ public interface CardRepository extends JpaRepository<Card, Long> {
      * 실제 카드 번호를 통해 카드를 조회하는 쿼리메서드
      */
     Optional<Card> findByRealCardNumber(String realCardNumber);
+
+    /**
+     * 카드 삭제 쿼리메서드
+     * @param id
+     * @param memberId
+     * @return 삭제된 레코드 개수
+     */
+    int deleteByIdAndMemberId(Long id, Long memberId);
 }
