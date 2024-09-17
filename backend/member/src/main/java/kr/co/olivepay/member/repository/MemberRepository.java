2205daa6668c6091dc3a483ca08652d04448b216
wnso.kim <wnso.kim@gmail.com>
2024-09-18 01:14:43 +0900

    ✨ feat: 전화번호 중복확인 Repository 개발
    - #S11P21A601-339

---
+++ backend/member/src/main/java/kr/co/olivepay/member/repository/MemberRepository.java
@@ -0,0 +1,13 @@
+package kr.co.olivepay.member.repository;
+
+import kr.co.olivepay.member.entity.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface MemberRepository extends JpaRepository<Member, Long> {
+
+    Optional<Member> findByPhoneNumber(final String phoneNumber);
+}
