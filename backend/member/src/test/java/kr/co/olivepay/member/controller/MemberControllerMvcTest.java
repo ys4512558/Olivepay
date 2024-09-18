26a6f7e4299402c70dbc8083ff3890378915afa8
wnso.kim <wnso.kim@gmail.com>
2024-09-18 22:24:20 +0900

    ✨ Feat: 테스트에서 Spring Security filter 제거
    - #S11P21A601-336

--- backend/member/src/test/java/kr/co/olivepay/member/controller/MemberControllerMvcTest.java
+++ backend/member/src/test/java/kr/co/olivepay/member/controller/MemberControllerMvcTest.java
@@ -6,6 +6,7 @@ import kr.co.olivepay.member.service.MemberService;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.TestPropertySource;
@@ -21,6 +22,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @WebMvcTest(MemberController.class)
 @TestPropertySource(properties = {"server.port=8080"})
+@AutoConfigureMockMvc(addFilters = false) // Security Filter 비활성화
 public class MemberControllerMvcTest {
 
     @MockBean
