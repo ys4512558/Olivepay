a57adb9906d3a75af22886d43e5d06c39977702e
wnso.kim <wnso.kim@gmail.com>
2024-09-18 21:19:58 +0900

    ✨ Feat: Member 등록 Service 구현
    - #S11P21A601-336
    - 비밀번호 저장 시 Spring Security Encoder 사용

--- backend/member/src/main/java/kr/co/olivepay/member/service/MemberService.java
+++ backend/member/src/main/java/kr/co/olivepay/member/service/MemberService.java
@@ -1,10 +1,15 @@
 package kr.co.olivepay.member.service;
 
 
-import kr.co.olivepay.member.dto.DuplicateRes;
+import kr.co.olivepay.member.dto.res.DuplicateRes;
+import kr.co.olivepay.member.dto.req.MemberRegisterReq;
+import kr.co.olivepay.member.entity.Member;
+import kr.co.olivepay.member.enums.Role;
 import kr.co.olivepay.member.global.response.SuccessResponse;
 
 public interface MemberService {
+
+    Member registerMember(MemberRegisterReq request, Role role);
 
     SuccessResponse<DuplicateRes> checkPhoneNumberDuplicate(String phoneNumber);
 }
--- backend/member/src/main/java/kr/co/olivepay/member/service/impl/MemberServiceImpl.java
+++ backend/member/src/main/java/kr/co/olivepay/member/service/impl/MemberServiceImpl.java
@@ -1,24 +1,56 @@
 package kr.co.olivepay.member.service.impl;
 
-import kr.co.olivepay.member.dto.DuplicateRes;
+import kr.co.olivepay.member.dto.res.DuplicateRes;
+import kr.co.olivepay.member.dto.req.MemberRegisterReq;
+import kr.co.olivepay.member.entity.Member;
+import kr.co.olivepay.member.enums.Role;
+import kr.co.olivepay.member.global.handler.AppException;
 import kr.co.olivepay.member.global.response.SuccessResponse;
+import kr.co.olivepay.member.mapper.MemberMapper;
 import kr.co.olivepay.member.repository.MemberRepository;
 import kr.co.olivepay.member.service.MemberService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import static kr.co.olivepay.member.global.enums.ErrorCode.*;
 import static kr.co.olivepay.member.global.enums.SuccessCode.*;
 
 @Service
 @RequiredArgsConstructor
 public class MemberServiceImpl implements MemberService {
 
+    private final PasswordEncoder passwordEncoder;
     private final MemberRepository memberRepository;
+    private final MemberMapper memberMapper;
 
     @Override
-    public SuccessResponse<DuplicateRes> checkPhoneNumberDuplicate(String phoneNumber) {
+    public Member registerMember(MemberRegisterReq request, Role role){
+        // 전화번호 중복 확인
+        validateByPhoneNumber(request.phoneNumber());
+
+        // Member Entity
+        Member member =
+                memberMapper.toMember(request, passwordEncoder.encode(request.password()), role);
+
+        return memberRepository.save(member);
+    }
+
+    @Override
+    public SuccessResponse<DuplicateRes> checkPhoneNumberDuplicate(final String phoneNumber) {
         Boolean isDuplicated = memberRepository.existsByPhoneNumber(phoneNumber);
         DuplicateRes response = new DuplicateRes(isDuplicated);
         return new SuccessResponse<>(CHECK_PHONE_NUMBER_DUPLICATE_SUCCESS, response);
+    }
+
+    /**
+     * 전화번호 중복 검사 메소드<br>
+     * 중복 시 PHONE_NUMBER_DUPLICATED Error
+     * @param phoneNumber 11자리 전화번호
+     */
+    private void validateByPhoneNumber(final String phoneNumber){
+        if(memberRepository.existsByPhoneNumber(phoneNumber)){
+            throw new AppException(PHONE_NUMBER_DUPLICATED);
+        }
     }
 }
