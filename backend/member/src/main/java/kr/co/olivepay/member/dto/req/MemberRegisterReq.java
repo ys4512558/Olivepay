32345d1ebe107378b97e8a75351d64cfe81e9b3b
wnso.kim <wnso.kim@gmail.com>
2024-09-19 01:02:59 +0900

    ♻️ Refactor: 불필요한 Annotation(NotBlank) 삭제
    - #S11P21A601-336
    - Size가 있으므로, NotBlack 삭제

--- backend/member/src/main/java/kr/co/olivepay/member/dto/req/MemberRegisterReq.java
+++ backend/member/src/main/java/kr/co/olivepay/member/dto/req/MemberRegisterReq.java
@@ -7,18 +7,15 @@ import lombok.Builder;
 
 @Builder
 public record MemberRegisterReq (
-        @NotBlank(message = "이름은 공백일 수 없습니다.")
-        @Size(max = 30, message = "이름은 최대 30자 입니다.")
+        @Size(min = 3, max = 30, message = "이름은 최소 3자, 최대 30자 입니다.")
         String name,
 
-        @NotBlank(message = "비밀번호는 공백일 수 없습니다.")
         @Pattern(
                 regexp = "^(?=.*[A-Za-z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{8,16}$",
                 message = "비밀번호 형식이 유효하지 않습니다."
         )
         String password,
 
-        @NotBlank(message = "전화번호는 공백일 수 없습니다.")
         @Size(min = 11, max = 11, message = "전화번호는 11자 입니다.")
         String phoneNumber
 ) { }
--- backend/member/src/main/java/kr/co/olivepay/member/dto/req/UserRegisterReq.java
+++ backend/member/src/main/java/kr/co/olivepay/member/dto/req/UserRegisterReq.java
@@ -7,15 +7,12 @@ import lombok.Builder;
 
 @Builder
 public record UserRegisterReq(
-        @NotBlank(message = "닉네임은 공백일 수 없습니다.")
-        @Size(max = 30, message = "닉네임은 최대 30자 입니다.")
+        @Size(min = 3, max = 30, message = "닉네임은 최대 30자 입니다.")
         String nickname,
 
-        @NotBlank(message = "생년월일은 공백일 수 없습니다.")
         @Size(min = 8, max = 8, message = "생년월일은 8자 입니다.")
         String birthdate,
 
-        @NotBlank(message = "간편 결제 비밀번호는 공백일 수 없습니다.")
         @Size(min = 6, max = 6, message = "간편 결제 비밀번호는 6자 입니다.")
         String pin,
 
