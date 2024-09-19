edbc1f33b7e4593668ef5410dddb4085cd311a64
김진이 <jinniek48@gmail.com>
2024-09-19 18:37:19 +0900

    ✨ Feat: LikeController 구현
    -#S11P21A601-366

---
+++ backend/franchise/src/main/java/kr/co/olivepay/franchise/controller/LikeController.java
@@ -0,0 +1,48 @@
+package kr.co.olivepay.franchise.controller;
+
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import io.swagger.v3.oas.annotations.Operation;
+import kr.co.olivepay.franchise.dto.res.LikedFranchiseRes;
+import kr.co.olivepay.franchise.global.enums.NoneResponse;
+import kr.co.olivepay.franchise.global.response.Response;
+import kr.co.olivepay.franchise.global.response.SuccessResponse;
+import kr.co.olivepay.franchise.service.LikeService;
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequestMapping("/franchises/likes")
+@RequiredArgsConstructor
+public class LikeController {
+
+	private final LikeService likeService;
+
+	@GetMapping("/user")
+	@Operation(description = """
+		내가 좋아요를 누른 모든 가맹점을 조회합니다.
+		가맹점 id와 가맹점 상호명 List를 반환합니다.
+		""", summary = "내가 좋아하는 가맹점 조회")
+	public ResponseEntity<Response<List<LikedFranchiseRes>>> getLikedFranchiseList(
+	) {
+		Long memberId = 1L; //TODO: auth 처리
+		SuccessResponse<List<LikedFranchiseRes>> response = likeService.getLikedFranchiseList(memberId);
+		return Response.success(response);
+	}
+
+	@PostMapping("/{franchiseId}")
+	@Operation(summary = "가맹점에 대한 좋아요 토글")
+	public ResponseEntity<Response<NoneResponse>> toggleLike(
+		@PathVariable Long franchiseId) {
+		Long memberId = 1L; //TODO: auth 처리
+		SuccessResponse<NoneResponse> response = likeService.toggleLike(memberId, franchiseId);
+		return Response.success(response);
+	}
+
+}
