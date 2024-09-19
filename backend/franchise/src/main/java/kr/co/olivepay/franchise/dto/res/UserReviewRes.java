5a20c922a90d3aff931c35c628b7991b93c938ba
김진이 <jinniek48@gmail.com>
2024-09-19 15:02:12 +0900

    ♻️ Refactor: user, owner를 member로 통일

--- backend/franchise/src/main/java/kr/co/olivepay/franchise/dto/res/UserReviewRes.java
+++ backend/franchise/src/main/java/kr/co/olivepay/franchise/dto/res/UserReviewRes.java
@@ -5,7 +5,7 @@ import lombok.Builder;
 /**
  * 가맹점 상세 검색 시 사용할 리뷰 dto
  * @param id 리뷰 id
- * @param userId 사용자 id
+ * @param memberId 사용자 id
  * @param userName 사용자 이름
  * @param stars 별점
  * @param content 내용
@@ -13,7 +13,7 @@ import lombok.Builder;
 @Builder
 public record UserReviewRes(
 	Long id,
-	Long userId,
+	Long memberId,
 	String userName,
 	Integer stars,
 	String content
