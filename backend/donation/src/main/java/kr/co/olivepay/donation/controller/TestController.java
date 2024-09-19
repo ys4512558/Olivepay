eae8707f568c6a6e5991ea6c73586643b1a9f5ed
서정희 <sjhlko@naver.com>
2024-09-19 13:13:51 +0900

    🐛 Fix: yml 및 테스트 컨트롤러 수정

--- backend/donation/src/main/java/kr/co/olivepay/donation/controller/TestController.java
+++ backend/donation/src/main/java/kr/co/olivepay/donation/controller/TestController.java
@@ -9,7 +9,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@RestController("/donations")
 public class TestController {
     @GetMapping("/test")
     public ResponseEntity<Response<NoneResponse>> test() {
