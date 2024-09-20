a47a3164ffdbda493ad72897ad00a5f156b6651b
ys4512558@naver.com <ys4512558@naver.com>
2024-09-20 15:05:41 +0900

    :sparkles: Feat: FeignClient 반환 dto 개발 및 반환 값 변경

--- backend/card/src/main/java/kr/co/olivepay/card/client/MemberServiceClient.java
+++ backend/card/src/main/java/kr/co/olivepay/card/client/MemberServiceClient.java
@@ -1,14 +1,15 @@
 package kr.co.olivepay.card.client;
 
+import kr.co.olivepay.card.client.dto.req.UserKeyRes;
 import kr.co.olivepay.card.global.response.Response;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-@FeignClient(name = "member")
+@FeignClient(name = "member", url = "${config.backend}:8000/members")
 public interface MemberServiceClient {
 
-    @GetMapping("/members/users/user-key")
-    Response<String> getUserKey(@RequestParam("memberId") Long memberId);
+    @GetMapping("/users/user-key")
+    Response<UserKeyRes> getUserKey(@RequestParam("memberId") Long memberId);
 
 }
