33719ab060fa52d9241c9a8c2b7b4f39a1a75991
dlthgml1501 <dlthgml1501@gmail.com>
2024-09-20 13:57:46 +0900

    :sparkles: Feat: bg-DARK 추가

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -11,6 +11,7 @@ export default {
         PRIMARY: '#99BBA2',
         SECONDARY: '#C6D6B2',
         TERTIARY: '#739E93',
+        DARK: '#3E646F',
         BASE: '#AFAFAF',
         LIGHTBASE: '#DADADA',
         DARKBASE: '#6F6F6F',
