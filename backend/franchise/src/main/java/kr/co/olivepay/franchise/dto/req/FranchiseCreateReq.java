06d12375dcb9f9112036f9d3a0573cc3a89027f8
김진이 <jinniek48@gmail.com>
2024-09-20 14:34:53 +0900

    🐛 Fix: dto에 위도, 경도 추가

--- backend/franchise/src/main/java/kr/co/olivepay/franchise/dto/req/FranchiseCreateReq.java
+++ backend/franchise/src/main/java/kr/co/olivepay/franchise/dto/req/FranchiseCreateReq.java
@@ -1,6 +1,10 @@
 package kr.co.olivepay.franchise.dto.req;
 
+import jakarta.validation.constraints.DecimalMax;
+import jakarta.validation.constraints.DecimalMin;
+import jakarta.validation.constraints.Digits;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.Builder;
 
@@ -24,6 +28,17 @@ public record FranchiseCreateReq(
 
 	@NotBlank(message = "주소는 공백일 수 없습니다.")
 	@Size(max = 50, message = "주소는 최대 50자까지 입력 가능합니다.")
-	String address
+	String address,
+
+	@NotNull(message = "위도는 필수 입력값입니다.")
+	@DecimalMin(value = "-90.0", message = "위도는 -90.0 이상이어야 합니다.")
+	@DecimalMax(value = "90.0", message = "위도는 90.0 이하여야 합니다.")
+	Float latitude,
+
+	@NotNull(message = "경도는 필수 입력값입니다.")
+	@DecimalMin(value = "-180.0", message = "경도는 -180.0 이상이어야 합니다.")
+	@DecimalMax(value = "180.0", message = "경도는 180.0 이하여야 합니다.")
+	Float longitude
+
 ) {
 }
--- backend/franchise/src/main/java/kr/co/olivepay/franchise/dto/res/FranchiseBasicRes.java
+++ backend/franchise/src/main/java/kr/co/olivepay/franchise/dto/res/FranchiseBasicRes.java
@@ -6,6 +6,8 @@ import lombok.Builder;
  * 조건에 맞는 가맹점 검색을 위한 dto
  * @param franchiseId 가맹점 id
  * @param franchiseName 상호명
+ * @param latitude 위도
+ * @param longitude 경도
  * @param category 카테고리
  * @param likes 좋아요 개수
  * @param avgStars 평균 별점
@@ -16,6 +18,8 @@ import lombok.Builder;
 public record FranchiseBasicRes (
 	Long franchiseId,
 	String franchiseName,
+	Float latitude,
+	Float longitude,
 	String category,
 	Integer likes,
 	Integer coupons,
