9c9df61093b7b6b3619a478a1b8cefc23bbb59c7
dlthgml1501 <dlthgml1501@gmail.com>
2024-09-20 13:58:19 +0900

    :sparkles: Feat: 후원 정보 페이지 생성

--- frontend/src/pages/DonationInfoPage.tsx
+++ frontend/src/pages/DonationInfoPage.tsx
@@ -1,15 +1,49 @@
-import { Layout, PageTitle, BackButton } from '../component/common';
+import { Layout, PageTitle } from '../component/common';
+import {
+  MapIcon,
+  ChartPieIcon,
+  FolderIcon,
+  TicketIcon,
+} from '@heroicons/react/24/solid';
 
 const DonationInfoPage = () => {
   return (
     <Layout>
-      <header className="flex items-center justify-between px-10 pb-10 pt-24">
-        <BackButton />
-        <div className="flex-grow text-center">
+      <header className="flex items-center justify-center pb-10 pt-24">
+        <div className="text-center">
           <PageTitle title="후원 정보 페이지" />
         </div>
-        <div className="w-8" />
       </header>
+      <div className="flex flex-col gap-y-10">
+        <section className="text-white">
+          <div className="bg-DARK flex flex-col gap-y-4 p-6 text-center">
+            <h2 className="">✨기부금 현황</h2>
+            <p className="text-xl">총 기부금: 5,620,000원</p>
+            <p>끼니수 : 200</p>
+          </div>
+        </section>
+        <section className="grid grid-cols-2 gap-4 p-4">
+          <div className="flex flex-col items-center gap-y-6 rounded-lg bg-white p-2 shadow">
+            <TicketIcon className="size-8 text-BASE" />
+            <span>내 후원 조회하기</span>
+          </div>
+          <div className="flex flex-col items-center gap-y-6 rounded-lg bg-white p-2 shadow">
+            <MapIcon className="size-8 text-BASE" />
+            <span>가맹점 조회하기</span>
+          </div>
+          <div className="flex flex-col items-center gap-y-6 rounded-lg bg-white p-2 shadow">
+            <ChartPieIcon className="size-8 text-BASE" />
+            <span>아동 선호 음식</span>
+          </div>
+          <div className="flex flex-col items-center gap-y-6 rounded-lg bg-white p-2 shadow">
+            <FolderIcon className="size-8 text-BASE" />
+            <span>관련 기사</span>
+          </div>
+        </section>
+        <footer className="bg-LIGHTBASE p-4 text-center">
+          <p className="text-black">캐러셀</p>
+        </footer>
+      </div>
     </Layout>
   );
 };
