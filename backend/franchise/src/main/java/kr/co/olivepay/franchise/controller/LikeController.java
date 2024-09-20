a7f1df1626f5873ed1fdc032d3ecc289af4ae953
김진이 <jinniek48@gmail.com>
2024-09-20 10:46:27 +0900

    ✨ Feat: Like Controller 더미 데이터 추가
    -#S11P21A601-366

--- backend/franchise/src/main/java/kr/co/olivepay/franchise/controller/LikeController.java
+++ backend/franchise/src/main/java/kr/co/olivepay/franchise/controller/LikeController.java
@@ -1,5 +1,6 @@
 package kr.co.olivepay.franchise.controller;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.http.ResponseEntity;
@@ -10,8 +11,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import io.swagger.v3.oas.annotations.Operation;
+import kr.co.olivepay.franchise.dto.res.FranchiseBasicRes;
+import kr.co.olivepay.franchise.dto.res.FranchiseMinimalRes;
 import kr.co.olivepay.franchise.dto.res.LikedFranchiseRes;
+import kr.co.olivepay.franchise.entity.Category;
 import kr.co.olivepay.franchise.global.enums.NoneResponse;
+import kr.co.olivepay.franchise.global.enums.SuccessCode;
 import kr.co.olivepay.franchise.global.response.Response;
 import kr.co.olivepay.franchise.global.response.SuccessResponse;
 import kr.co.olivepay.franchise.service.LikeService;
@@ -22,7 +27,7 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class LikeController {
 
-	private final LikeService likeService;
+	//private final LikeService likeService;
 
 	@GetMapping("/user")
 	@Operation(description = """
@@ -31,8 +36,32 @@ public class LikeController {
 		""", summary = "내가 좋아하는 가맹점 조회")
 	public ResponseEntity<Response<List<LikedFranchiseRes>>> getLikedFranchiseList(
 	) {
-		Long memberId = 1L; //TODO: auth 처리
-		SuccessResponse<List<LikedFranchiseRes>> response = likeService.getLikedFranchiseList(memberId);
+		// Long memberId = 1L; //TODO: auth 처리
+		// SuccessResponse<List<LikedFranchiseRes>> response = likeService.getLikedFranchiseList(memberId);
+
+		FranchiseMinimalRes franchise1 = FranchiseMinimalRes.builder()
+															.id(1L)
+															.name("멀티 캠퍼스")
+															.build();
+		LikedFranchiseRes dto1 = LikedFranchiseRes.builder()
+												  .likeId(11111L)
+												  .franchise(franchise1)
+												  .build();
+
+		FranchiseMinimalRes franchise2 = FranchiseMinimalRes.builder()
+															.id(2L)
+															.name("아웃백 스테이크하우스")
+															.build();
+		LikedFranchiseRes dto2 = LikedFranchiseRes.builder()
+												  .likeId(222222L)
+												  .franchise(franchise2)
+												  .build();
+
+		List<LikedFranchiseRes> dtoList = new ArrayList<>();
+		dtoList.add(dto1);
+		dtoList.add(dto2);
+
+		SuccessResponse<List<LikedFranchiseRes>> response = new SuccessResponse<>(SuccessCode.LIKED_FRANCHISE_SEARCH_SUCCESS, dtoList);
 		return Response.success(response);
 	}
 
@@ -40,8 +69,11 @@ public class LikeController {
 	@Operation(summary = "가맹점에 대한 좋아요 토글")
 	public ResponseEntity<Response<NoneResponse>> toggleLike(
 		@PathVariable Long franchiseId) {
-		Long memberId = 1L; //TODO: auth 처리
-		SuccessResponse<NoneResponse> response = likeService.toggleLike(memberId, franchiseId);
+		// Long memberId = 1L; //TODO: auth 처리
+		// SuccessResponse<NoneResponse> response = likeService.toggleLike(memberId, franchiseId);
+
+		SuccessResponse<NoneResponse> response = new SuccessResponse<>(SuccessCode.LIKE_TOGGLE_SUCCESS, NoneResponse.NONE);
+
 		return Response.success(response);
 	}
 
