917ad4534bbaf5b4e439ec70cd14516f4528cdad
ys4512558@naver.com <ys4512558@naver.com>
2024-09-23 09:58:12 +0900

    :recycle: Refactor: 요청 형식 Header추가 및 gateway-port 추가

--- backend/card/src/main/java/kr/co/olivepay/card/client/MemberServiceClient.java
+++ backend/card/src/main/java/kr/co/olivepay/card/client/MemberServiceClient.java
@@ -4,12 +4,14 @@ import kr.co.olivepay.card.client.dto.req.UserKeyRes;
 import kr.co.olivepay.card.global.response.Response;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestHeader;
 
-@FeignClient(name = "member", url = "${config.backend}:8000/members")
+@FeignClient(name = "member", url = "${config.backend}:${config.gateway-port}/members")
 public interface MemberServiceClient {
 
+    String MEMBER_ID = "memberId";
+
     @GetMapping("/users/user-key")
-    Response<UserKeyRes> getUserKey(@RequestParam("memberId") Long memberId);
+    Response<UserKeyRes> getUserKey(@RequestHeader(MEMBER_ID) Long memberId);
 
 }
