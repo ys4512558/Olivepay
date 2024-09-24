989533e701d80a7c11d7b665df49dc6197e5323e
김진이 <jinniek48@gmail.com>
2024-09-24 18:29:08 +0900

    ✨ Feat: Mapper 구현

--- backend/franchise/src/main/java/kr/co/olivepay/franchise/mapper/LikeMapper.java
+++ backend/franchise/src/main/java/kr/co/olivepay/franchise/mapper/LikeMapper.java
@@ -4,6 +4,7 @@ import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 
 import kr.co.olivepay.franchise.entity.Franchise;
+import kr.co.olivepay.franchise.dto.res.LikedFranchiseRes;
 import kr.co.olivepay.franchise.entity.Like;
 
 @Mapper(componentModel = "spring")
@@ -11,5 +12,9 @@ public interface LikeMapper {
 
 	@Mapping(source = "franchise", target="franchise")
 	Like toEntity(Long memberId, Franchise franchise);
+
+	@Mapping(source = "like.id", target = "likeId")
+	LikedFranchiseRes toLikedFranchiseRes(Like like);
+
 
 }
