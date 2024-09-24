d01d66b4b9211dd9449bea4aad17b38cffe397bd
왕승철 <ys4512558@naver.com>
2024-09-24 23:35:55 +0900

    ✨ Feat: 카프카 이벤트 발행 서비스 개발
    - S11P21A601-437

---
+++ backend/card/src/main/java/kr/co/olivepay/card/transaction/publisher/KafkaTransactionEventPublisher.java
@@ -0,0 +1,30 @@
+package kr.co.olivepay.card.transaction.publisher;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.support.SendResult;
+import org.springframework.stereotype.Component;
+
+import java.util.concurrent.CompletableFuture;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class KafkaTransactionEventPublisher implements TransactionEventPublisher{
+
+    private final KafkaTemplate<String, String> kafkaTemplate;
+    private final ObjectMapper objectMapper;
+
+    @Override
+    public CompletableFuture<SendResult<String, String>> publishEvent(final String topic, final String key, Object event) {
+        try {
+            return kafkaTemplate.send(topic, key, objectMapper.writeValueAsString(event));
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+}
