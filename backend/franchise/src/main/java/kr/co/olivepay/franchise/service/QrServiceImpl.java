b2f8217b34471e0fa2b0a402faca93258a3ae733
김진이 <jinniek48@gmail.com>
2024-09-24 12:34:10 +0900

    ✨ Feat: QR 코드 이미지 생성 Service 구현

---
+++ backend/franchise/src/main/java/kr/co/olivepay/franchise/service/QrServiceImpl.java
@@ -0,0 +1,42 @@
+package kr.co.olivepay.franchise.service;
+
+import java.io.ByteArrayOutputStream;
+import java.util.Base64;
+
+import org.springframework.stereotype.Service;
+
+import kr.co.olivepay.franchise.dto.res.QrCodeRes;
+import kr.co.olivepay.franchise.global.enums.ErrorCode;
+import kr.co.olivepay.franchise.global.enums.SuccessCode;
+import kr.co.olivepay.franchise.global.handler.AppException;
+import kr.co.olivepay.franchise.global.response.SuccessResponse;
+import kr.co.olivepay.franchise.mapper.FranchiseMapper;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.client.j2se.MatrixToImageWriter;
+import com.google.zxing.common.BitMatrix;
+import com.google.zxing.qrcode.QRCodeWriter;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class QrServiceImpl implements QrService{
+
+	private final FranchiseMapper franchiseMapper;
+
+	@Override
+	public SuccessResponse<QrCodeRes> getQrCode(Long franchiseId, Integer amount) {
+		try {
+			QRCodeWriter qrCodeWriter = new QRCodeWriter();
+			ByteArrayOutputStream pngOutputStream = new ByteArrayOutputStream();
+			BitMatrix bitMatrix = qrCodeWriter.encode(String.format("franchiseId=%d&amount=%d", franchiseId, amount), BarcodeFormat.QR_CODE, 300, 300);
+			MatrixToImageWriter.writeToStream(bitMatrix, "PNG", pngOutputStream);
+			QrCodeRes response = franchiseMapper.toQrCodeRes(Base64.getEncoder().encodeToString(pngOutputStream.toByteArray()));
+			return new SuccessResponse<>(SuccessCode.QR_CREATE_SUCCESS, response);
+		} catch (Exception e) {
+			throw new AppException(ErrorCode.QR_CREATE_ERROR);
+		}
+	}
+}
