a5ab5b4005d6ec4019e11bc5cb0b2094738a31ff
김진이 <jinniek48@gmail.com>
2024-09-24 10:22:11 +0900

    ✨feat: 가맹점 상세 조회 로직 구현

--- backend/franchise/src/main/java/kr/co/olivepay/franchise/controller/FranchiseController.java
+++ backend/franchise/src/main/java/kr/co/olivepay/franchise/controller/FranchiseController.java
@@ -2,7 +2,6 @@ package kr.co.olivepay.franchise.controller;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,12 +14,9 @@ import org.springframework.web.bind.annotation.RestController;
 
 import io.swagger.v3.oas.annotations.Operation;
 import jakarta.validation.Valid;
-import kr.co.olivepay.franchise.dto.req.FranchiseCreateReq;
-import kr.co.olivepay.franchise.dto.res.FranchiseBasicRes;
-import kr.co.olivepay.franchise.dto.res.FranchiseDetailRes;
-import kr.co.olivepay.franchise.dto.res.FranchiseMinimalRes;
-import kr.co.olivepay.franchise.dto.res.QrCodeRes;
-import kr.co.olivepay.franchise.entity.Category;
+import kr.co.olivepay.franchise.dto.req.*;
+import kr.co.olivepay.franchise.dto.res.*;
+import kr.co.olivepay.franchise.entity.*;
 import kr.co.olivepay.franchise.global.enums.NoneResponse;
 import kr.co.olivepay.franchise.global.enums.SuccessCode;
 import kr.co.olivepay.franchise.global.response.Response;
@@ -35,7 +31,7 @@ import lombok.RequiredArgsConstructor;
 public class FranchiseController {
 
 	private final FranchiseService franchiseService;
-	//private final QrService qrService;
+	private final QrService qrService;
 
 	@PostMapping("/owner")
 	@Operation(description = """
--- backend/franchise/src/main/java/kr/co/olivepay/franchise/service/FranchiseServiceImpl.java
+++ backend/franchise/src/main/java/kr/co/olivepay/franchise/service/FranchiseServiceImpl.java
@@ -1,15 +1,15 @@
 package kr.co.olivepay.franchise.service;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
-import jakarta.transaction.Transactional;
 import kr.co.olivepay.franchise.global.enums.ErrorCode;
 import kr.co.olivepay.franchise.global.enums.NoneResponse;
 import kr.co.olivepay.franchise.global.enums.SuccessCode;
 import kr.co.olivepay.franchise.global.handler.AppException;
-import kr.co.olivepay.franchise.global.response.Response;
 import kr.co.olivepay.franchise.global.response.SuccessResponse;
 import lombok.RequiredArgsConstructor;
 
@@ -58,9 +58,30 @@ public class FranchiseServiceImpl implements FranchiseService{
 		return null;
 	}
 
+	/**
+	 * 가맹점 상세 정보 조회
+	 * @param franchiseId
+	 * @return
+	 */
 	@Override
+	@Transactional(readOnly = true)
 	public SuccessResponse<FranchiseDetailRes> getFranchiseDetail(Long franchiseId) {
-		return null;
+		Franchise franchise = franchiseRepository.findById(franchiseId)
+			.orElseThrow(() -> new AppException(ErrorCode.FRANCHISE_NOT_FOUND_BY_ID));
+
+		//TODO: Coupon 서비스와 연결
+		Integer coupon2 = 1;
+		Integer coupon4 = 2;
+
+		//TODO: Like 서비스와 연결
+		Integer likes = 123;
+		//Integer likes = likeService.getLikesCount(franchiseId);
+
+		//TODO: LikeService에 좋아요 눌렀는지 아닌지 조회하는 거 구현하기
+		Boolean isLiked = true;
+
+		FranchiseDetailRes response = franchiseMapper.toFranchiseDetailRes(franchise, coupon2, coupon4, likes, isLiked);
+		return new SuccessResponse<>(SuccessCode.FRANCHISE_DETAIL_SUCCESS, response);
 	}
 
 	@Override
