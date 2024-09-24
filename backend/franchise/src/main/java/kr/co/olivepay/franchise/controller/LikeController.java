f724d3dd2c2cf8516c323c0d7215d52062bbd7e4
김진이 <jinniek48@gmail.com>
2024-09-24 18:28:48 +0900

    ✨ Feat: Controller 구현

--- backend/franchise/src/main/java/kr/co/olivepay/franchise/controller/LikeController.java
+++ backend/franchise/src/main/java/kr/co/olivepay/franchise/controller/LikeController.java
@@ -36,32 +36,8 @@ public class LikeController {
 		""", summary = "내가 좋아하는 가맹점 조회")
 	public ResponseEntity<Response<List<LikedFranchiseRes>>> getLikedFranchiseList(
 	) {
-		// Long memberId = 1L; //TODO: auth 처리
-		// SuccessResponse<List<LikedFranchiseRes>> response = likeService.getLikedFranchiseList(memberId);
-
-		FranchiseMinimalRes franchise1 = FranchiseMinimalRes.builder()
-															.id(1L)
-															.name("멀티 캠퍼스")
-															.build();
-		LikedFranchiseRes dto1 = LikedFranchiseRes.builder()
-												  .likeId(11111L)
-												  .franchise(franchise1)
-												  .build();
-
-		FranchiseMinimalRes franchise2 = FranchiseMinimalRes.builder()
-															.id(2L)
-															.name("아웃백 스테이크하우스")
-															.build();
-		LikedFranchiseRes dto2 = LikedFranchiseRes.builder()
-												  .likeId(222222L)
-												  .franchise(franchise2)
-												  .build();
-
-		List<LikedFranchiseRes> dtoList = new ArrayList<>();
-		dtoList.add(dto1);
-		dtoList.add(dto2);
-
-		SuccessResponse<List<LikedFranchiseRes>> response = new SuccessResponse<>(SuccessCode.LIKED_FRANCHISE_SEARCH_SUCCESS, dtoList);
+		Long memberId = 1L; //TODO: auth 처리
+		SuccessResponse<List<LikedFranchiseRes>> response = likeService.getLikedFranchiseList(memberId);
 		return Response.success(response);
 	}
 
