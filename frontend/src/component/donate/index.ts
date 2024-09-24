0d324c973a85149bec79bec4fdca9199c4a5b135
dlthgml1501 <dlthgml1501@gmail.com>
2024-09-24 17:12:35 +0900

    :sparkles: Feat: 후원 페이지

---
+++ frontend/src/component/donate/index.ts
@@ -0,0 +1,5 @@
+export { default as Donate1 } from './Donate1';
+export { default as Donate2 } from './Donate2';
+export { default as Donate3 } from './Donate3';
+export { default as Donate4 } from './Donate4';
+export { default as Donate5 } from './Donate5';
