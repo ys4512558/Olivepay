3e8bf89207798250751c95c70bb8208aff34e171
김진이 <jinniek48@gmail.com>
2024-09-24 14:52:33 +0900

    ♻️ Refactor: 공통 에러 처리 부분 분리

--- backend/franchise/src/main/java/kr/co/olivepay/franchise/repository/FranchiseRepository.java
+++ backend/franchise/src/main/java/kr/co/olivepay/franchise/repository/FranchiseRepository.java
@@ -6,9 +6,16 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import kr.co.olivepay.franchise.entity.Franchise;
+import kr.co.olivepay.franchise.global.enums.ErrorCode;
+import kr.co.olivepay.franchise.global.handler.AppException;
 
 @Repository
 public interface FranchiseRepository extends JpaRepository<Franchise, Long> {
 
 	Boolean existsByRegistrationNumber(String registrationNumber);
+
+	default Franchise getById(Long id){
+		return findById(id).orElseThrow(()->new AppException(ErrorCode.FRANCHISE_NOT_FOUND_BY_ID));
+	}
+
 }
