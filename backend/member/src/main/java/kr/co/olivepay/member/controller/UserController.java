cc36c565cedc3a680295456ad441cba7cd660cb1
wnso.kim <wnso.kim@gmail.com>
2024-09-24 20:11:53 +0900

    ✨ Feat: Member 서비스: 회원 권한 조정 API 개발 - 더미
    - #S11P21A601-409

--- backend/member/src/main/java/kr/co/olivepay/member/controller/UserController.java
+++ backend/member/src/main/java/kr/co/olivepay/member/controller/UserController.java
@@ -2,18 +2,22 @@ package kr.co.olivepay.member.controller;
 
 import io.swagger.v3.oas.annotations.Operation;
 import jakarta.validation.Valid;
+import kr.co.olivepay.core.member.dto.res.MemberRoleRes;
+import kr.co.olivepay.core.member.dto.res.UserKeyRes;
+import kr.co.olivepay.core.util.CommonUtil;
 import kr.co.olivepay.member.dto.req.UserRegisterReq;
-import kr.co.olivepay.member.dto.res.UserKeyRes;
 import kr.co.olivepay.member.global.enums.NoneResponse;
 import kr.co.olivepay.member.global.response.Response;
 import kr.co.olivepay.member.global.response.SuccessResponse;
 import kr.co.olivepay.member.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import static kr.co.olivepay.member.global.enums.SuccessCode.GET_USER_KEY_SUCCESS;
+import static kr.co.olivepay.member.global.enums.SuccessCode.USER_PROMOTE_SUCCESS;
 
 @RestController
 @RequiredArgsConstructor
@@ -42,6 +46,16 @@ public class UserController {
                                           .build();
         SuccessResponse<UserKeyRes> response = new SuccessResponse<>(GET_USER_KEY_SUCCESS, userKeyRes);
 
+        return Response.success(response);
+    }
+
+    @PatchMapping("/promote")
+    @Operation(description = "임시 회원을 일반 회원으로 권한을 조정합니다.", summary = "회원 권한 조정(내부 서버용) - 더미")
+    public ResponseEntity<Response<NoneResponse>> promoteUser(@RequestHeader HttpHeaders headers)
+    {
+        Long memberId = CommonUtil.getMemberId(headers);
+
+        SuccessResponse<NoneResponse> response = new SuccessResponse<>(USER_PROMOTE_SUCCESS,NoneResponse.NONE);
         return Response.success(response);
     }
 }
