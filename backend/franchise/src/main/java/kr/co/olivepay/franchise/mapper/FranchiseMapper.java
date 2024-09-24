e01279746e8d2123f676a32f62dbb55277f990ce
김진이 <jinniek48@gmail.com>
2024-09-24 12:35:50 +0900

    ✨ Feat: QrCodeRes Mapper 추가

--- backend/franchise/src/main/java/kr/co/olivepay/franchise/mapper/FranchiseMapper.java
+++ backend/franchise/src/main/java/kr/co/olivepay/franchise/mapper/FranchiseMapper.java
@@ -1,13 +1,11 @@
 package kr.co.olivepay.franchise.mapper;
 
-import java.math.BigDecimal;
-import java.math.RoundingMode;
-
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.Named;
 
 import kr.co.olivepay.franchise.dto.req.FranchiseCreateReq;
+import kr.co.olivepay.franchise.dto.res.QrCodeRes;
 import kr.co.olivepay.franchise.entity.Category;
 import kr.co.olivepay.franchise.entity.Franchise;
 
@@ -17,6 +15,7 @@ public interface FranchiseMapper {
 	@Mapping(source = "franchiseReq.category", target = "category", qualifiedByName = "stringToCategory")
 	Franchise toEntity(Long memberId, FranchiseCreateReq franchiseReq);
 
+	QrCodeRes toQrCodeRes(String image);
 
 	@Named("stringToCategory")
 	default Category stringToCategory(String category) {
