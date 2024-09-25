cd65a006acbdffe81084423b7c43857ec1b1185e
서정희 <sjhlko@naver.com>
2024-09-25 16:57:10 +0900

    💡 Docs: 자바독 추가

--- backend/donation/src/main/java/kr/co/olivepay/donation/service/DonationService.java
+++ backend/donation/src/main/java/kr/co/olivepay/donation/service/DonationService.java
@@ -2,8 +2,15 @@ package kr.co.olivepay.donation.service;
 
 import kr.co.olivepay.donation.dto.req.DonationReq;
 import kr.co.olivepay.donation.global.enums.NoneResponse;
+import kr.co.olivepay.donation.global.enums.SuccessCode;
 import kr.co.olivepay.donation.global.response.SuccessResponse;
 
 public interface DonationService {
+    /**
+     *
+     * 후원자의 정보를 받아 후원처리를 하는 메소드
+     * @param request 후원자 정보를 받는 DTO {@link DonationReq}
+     * @return 후원 성공 메시지 {@link SuccessCode}
+     */
     SuccessResponse<NoneResponse> donate(DonationReq request);
 }
