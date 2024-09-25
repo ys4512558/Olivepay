cb67e02b6d2eea10b2c014998defe38d08122c43
ys4512558@naver.com <ys4512558@naver.com>
2024-09-25 17:31:34 +0900

    :sparkles: Feat: 쿠폰 적용 상태 개발
    - S11P21A601-448

---
+++ backend/transaction/src/main/java/kr/co/olivepay/transaction/state/coupon/CouponUseSuccess.java
@@ -0,0 +1,12 @@
+package kr.co.olivepay.transaction.state.coupon;
+
+import kr.co.olivepay.transaction.PaymentSaga;
+import kr.co.olivepay.transaction.state.PaymentState;
+
+public class CouponUseSuccess implements PaymentState {
+
+    @Override
+    public void operate(PaymentSaga paymentSaga) {
+
+    }
+}
