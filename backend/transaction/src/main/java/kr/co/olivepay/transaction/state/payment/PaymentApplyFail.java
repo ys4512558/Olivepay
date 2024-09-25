390d64de23090ba0ab867c9626aa0547e005af64
ys4512558@naver.com <ys4512558@naver.com>
2024-09-25 17:31:23 +0900

    :sparkles: Feat: 결제 적용 상태 개발
    - S11P21A601-447

---
+++ backend/transaction/src/main/java/kr/co/olivepay/transaction/state/payment/PaymentApplyFail.java
@@ -0,0 +1,12 @@
+package kr.co.olivepay.transaction.state.payment;
+
+import kr.co.olivepay.transaction.PaymentSaga;
+import kr.co.olivepay.transaction.state.PaymentState;
+
+public class PaymentApplyFail implements PaymentState {
+
+    @Override
+    public void operate(PaymentSaga paymentSaga) {
+
+    }
+}
---
+++ backend/transaction/src/main/java/kr/co/olivepay/transaction/state/payment/PaymentApplySuccess.java
@@ -0,0 +1,12 @@
+package kr.co.olivepay.transaction.state.payment;
+
+import kr.co.olivepay.transaction.PaymentSaga;
+import kr.co.olivepay.transaction.state.PaymentState;
+
+public class PaymentApplySuccess implements PaymentState {
+
+    @Override
+    public void operate(PaymentSaga paymentSaga) {
+
+    }
+}
