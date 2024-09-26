3d4caa6b713c183ea4334fd1919bae0a6b72765d
ys4512558@naver.com <ys4512558@naver.com>
2024-09-26 16:04:08 +0900

    :recycle: Refactor: Config 환경변수 설정

--- backend/transaction/src/main/java/kr/co/olivepay/transaction/config/KafkaConsumerConfig.java
+++ backend/transaction/src/main/java/kr/co/olivepay/transaction/config/KafkaConsumerConfig.java
@@ -2,6 +2,7 @@ package kr.co.olivepay.transaction.config;
 
 import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.common.serialization.StringDeserializer;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
@@ -14,11 +15,18 @@ import java.util.Map;
 @Configuration
 public class KafkaConsumerConfig {
 
+    @Value("${config.kafka-server}")
+    private String KAFKA_SERVER;
+    @Value("${config.kafka-port}")
+    private String KAFKA_PORT;
+    @Value("${config.kafka-group-id-config}")
+    private String KAFKA_GROUP_ID_CONFIG;
+
     @Bean
     public ConsumerFactory<String, String> consumerFactory() {
         Map<String, Object> config = new HashMap<>();
-        config.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
-        config.put(ConsumerConfig.GROUP_ID_CONFIG, "payment_orchestrator");
+        config.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, KAFKA_SERVER + ":" + KAFKA_PORT);
+        config.put(ConsumerConfig.GROUP_ID_CONFIG, KAFKA_GROUP_ID_CONFIG);
         config.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
         config.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
         return new DefaultKafkaConsumerFactory<>(config);
--- backend/transaction/src/main/java/kr/co/olivepay/transaction/config/KafkaProducerConfig.java
+++ backend/transaction/src/main/java/kr/co/olivepay/transaction/config/KafkaProducerConfig.java
@@ -2,6 +2,7 @@ package kr.co.olivepay.transaction.config;
 
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.common.serialization.StringSerializer;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.core.DefaultKafkaProducerFactory;
@@ -14,10 +15,15 @@ import java.util.Map;
 @Configuration
 public class KafkaProducerConfig {
 
+    @Value("${config.kafka-server}")
+    private String KAFKA_SERVER;
+    @Value("${config.kafka-port}")
+    private String KAFKA_PORT;
+
     @Bean
     public ProducerFactory<String, String> producerFactory() {
         Map<String, Object> config = new HashMap<>();
-        config.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        config.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, KAFKA_SERVER + ":" + KAFKA_PORT);
         config.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
         config.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
         return new DefaultKafkaProducerFactory<>(config);
