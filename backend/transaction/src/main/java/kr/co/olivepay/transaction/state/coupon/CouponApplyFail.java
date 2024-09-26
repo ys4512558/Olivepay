44d500aee4fd1bc1f8e0b536d8d6065417e79f9d
ys4512558@naver.com <ys4512558@naver.com>
2024-09-26 15:54:58 +0900

    :recycle: Refactor: 쿠폰 적용 상태 클래스명 변경

--- backend/transaction/src/main/java/kr/co/olivepay/transaction/state/coupon/CouponApplyFail.java
+++ backend/transaction/src/main/java/kr/co/olivepay/transaction/state/coupon/CouponApplyFail.java
@@ -3,7 +3,7 @@ package kr.co.olivepay.transaction.state.coupon;
 import kr.co.olivepay.transaction.PaymentSaga;
 import kr.co.olivepay.transaction.state.PaymentState;
 
-public class CouponUseSuccess implements PaymentState {
+public class CouponApplyFail implements PaymentState {
 
     @Override
     public void operate(PaymentSaga paymentSaga) {
--- backend/transaction/src/main/java/kr/co/olivepay/transaction/state/coupon/CouponApplySuccess.java
+++ backend/transaction/src/main/java/kr/co/olivepay/transaction/state/coupon/CouponApplySuccess.java
@@ -3,7 +3,7 @@ package kr.co.olivepay.transaction.state.coupon;
 import kr.co.olivepay.transaction.PaymentSaga;
 import kr.co.olivepay.transaction.state.PaymentState;
 
-public class CouponUseFail implements PaymentState {
+public class CouponApplySuccess implements PaymentState {
 
     @Override
     public void operate(PaymentSaga paymentSaga) {
