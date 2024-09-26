7d409240c6d2131a4f106b729f9f248c04f683e0
ys4512558@naver.com <ys4512558@naver.com>
2024-09-26 15:56:19 +0900

    :recycle: Refactor: 계좌 잔액 체크 실패 이벤트 필드 추가

--- backend/transaction/src/main/java/kr/co/olivepay/transaction/state/account/AccountBalanceCheckFail.java
+++ backend/transaction/src/main/java/kr/co/olivepay/transaction/state/account/AccountBalanceCheckFail.java
@@ -1,9 +1,14 @@
 package kr.co.olivepay.transaction.state.account;
 
+import kr.co.olivepay.core.transaction.topic.Topic;
 import kr.co.olivepay.transaction.PaymentSaga;
 import kr.co.olivepay.transaction.state.PaymentState;
+import lombok.AllArgsConstructor;
 
+@AllArgsConstructor
 public class AccountBalanceCheckFail implements PaymentState {
+
+    private String failReason;
 
     @Override
     public void operate(PaymentSaga paymentSaga) {
