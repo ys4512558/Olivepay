99e0da74b7c6906804f1e9a55058cf7587b77ee9
ys4512558@naver.com <ys4512558@naver.com>
2024-09-26 13:03:41 +0900

    :sparkles: Feat: 결제 프로세스 성공/실패 이벤트 추가

--- backend/core/src/main/java/kr/co/olivepay/core/transaction/topic/Topic.java
+++ backend/core/src/main/java/kr/co/olivepay/core/transaction/topic/Topic.java
@@ -9,6 +9,10 @@ public class Topic {
 
     //결제 프로세스 시작
     public final static String PAYMENT_PENDING = "PAYMENT_PENDING";
+    //결제 프로세스 완료
+    public final static String PAYMENT_COMPLETE = "PAYMENT_COMPLETE";
+    //결제 프로세스 실패
+    public final static String PAYMENT_FAIL = "PAYMENT_FAIL";
 
     //잔액 체크
     public final static String ACCOUNT_BALANCE_CHECK = "ACCOUNT_BALANCE_CHECK";
@@ -24,5 +28,11 @@ public class Topic {
     //결제 실패
     public final static String PAYMENT_APPLY_FAIL = "PAYMENT_APPLY_FAIL";
 
+    //쿠폰 적용 요청
+    public final static String COUPON_APPLY = "COUPON_APPLY";
+    //쿠폰 적용 성공
+    public final static String COUPON_APPLY_SUCCESS = "COUPON_APPLY_SUCCESS";
+    //쿠폰 적용 실패
+    public final static String COUPON_APPLY_FAIL = "COUPON_APPLY_FAIL";
 }
 
