ca2ddb10bd25ed2e44278442d6483fc30934722a
서정희 <sjhlko@naver.com>
2024-09-27 14:01:26 +0900

    ♻️ Refactor: 큐 객체 static 으로 import 하도록 변경

--- backend/donation/src/main/java/kr/co/olivepay/donation/repository/impl/DonationRepositoryCustomImpl.java
+++ backend/donation/src/main/java/kr/co/olivepay/donation/repository/impl/DonationRepositoryCustomImpl.java
@@ -1,9 +1,11 @@
 package kr.co.olivepay.donation.repository.impl;
 
 import com.querydsl.jpa.impl.JPAQueryFactory;
-import kr.co.olivepay.donation.entity.QDonation;
 import kr.co.olivepay.donation.repository.DonationRepositoryCustom;
 import lombok.RequiredArgsConstructor;
+
+import static kr.co.olivepay.donation.entity.QDonation.donation;
+
 
 @RequiredArgsConstructor
 public class DonationRepositoryCustomImpl implements DonationRepositoryCustom {
@@ -12,7 +14,6 @@ public class DonationRepositoryCustomImpl implements DonationRepositoryCustom {
 
     @Override
     public Long sumMoney() {
-        QDonation donation = QDonation.donation;
         Long sum =  queryFactory.select(donation.money.sum())
                            .from(donation)
                            .fetchOne();
