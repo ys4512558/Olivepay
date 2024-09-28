6fb3ef665d8841395b7402834aeebb9a3ac53783
ys4512558@naver.com <ys4512558@naver.com>
2024-09-29 01:43:15 +0900

    :recycle: Refactor: 상태 변경 및 다음 단계 작업 메서드 변경

--- backend/transaction/src/main/java/kr/co/olivepay/transaction/listener/account/AccountBalanceCheckSuccessListener.java
+++ backend/transaction/src/main/java/kr/co/olivepay/transaction/listener/account/AccountBalanceCheckSuccessListener.java
@@ -2,12 +2,14 @@ package kr.co.olivepay.transaction.listener.account;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import kr.co.olivepay.core.transaction.topic.Topic;
 import kr.co.olivepay.transaction.PaymentSaga;
 import kr.co.olivepay.transaction.listener.KafkaEventListener;
 import kr.co.olivepay.transaction.repository.PaymentSagaRepository;
 import kr.co.olivepay.transaction.state.account.AccountBalanceCheckSuccess;
 import lombok.RequiredArgsConstructor;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -19,9 +21,11 @@ public class AccountBalanceCheckSuccessListener implements KafkaEventListener {
 
     /**
      * 계좌 잔액 체크 성공
+     *
      * @param record
      */
     @Override
+    @KafkaListener(topics = Topic.ACCOUNT_BALANCE_CHECK_SUCCESS, groupId = "payment-orchestrator")
     public void onMessage(ConsumerRecord<String, String> record) {
         try {
             String key = record.key();
@@ -29,8 +33,7 @@ public class AccountBalanceCheckSuccessListener implements KafkaEventListener {
             AccountBalanceCheckSuccess accountBalanceCheckSuccess
                     = objectMapper.readValue(value, AccountBalanceCheckSuccess.class);
             PaymentSaga paymentSaga = paymentSagaRepository.findById(key);
-            paymentSaga.setState(new AccountBalanceCheckSuccess());
-            paymentSaga.operate();
+            paymentSaga.setStateAndOperate(new AccountBalanceCheckSuccess());
         } catch (JsonProcessingException e) {
             throw new RuntimeException(e);
         }
