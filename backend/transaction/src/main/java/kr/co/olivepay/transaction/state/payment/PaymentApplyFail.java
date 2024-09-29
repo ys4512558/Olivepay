c7d9c4c8dfbf5f0d1abec3686748bd0ae177c2b6
ys4512558@naver.com <ys4512558@naver.com>
2024-09-29 23:26:21 +0900

    :sparkles: Feat: 결제 적용 성공/실패 상태 로직 구현
    - S11P21A601-505

--- backend/transaction/src/main/java/kr/co/olivepay/transaction/state/payment/PaymentApplyFail.java
+++ backend/transaction/src/main/java/kr/co/olivepay/transaction/state/payment/PaymentApplyFail.java
@@ -1,12 +1,46 @@
 package kr.co.olivepay.transaction.state.payment;
 
+import kr.co.olivepay.core.transaction.topic.Topic;
+import kr.co.olivepay.core.transaction.topic.event.payment.result.PaymentFailEvent;
+import kr.co.olivepay.core.transaction.topic.event.payment.result.PaymentRollbackEvent;
+import kr.co.olivepay.transaction.PaymentDetailSaga;
 import kr.co.olivepay.transaction.PaymentSaga;
+import kr.co.olivepay.transaction.mapper.PaymentDetailSagaMapper;
+import kr.co.olivepay.transaction.mapper.PaymentSagaMapper;
 import kr.co.olivepay.transaction.state.PaymentState;
+import lombok.AllArgsConstructor;
 
+import java.util.ArrayList;
+import java.util.List;
+
+@AllArgsConstructor
 public class PaymentApplyFail implements PaymentState {
 
+    private String failReason;
+
+    /**
+     * 실패 시 transactionUniqueNo != null인 롤백 이벤트를 결제 취소 요청하도록
+     * -> Payment_Fail(프로세스 종료)
+     *
+     * @param paymentSaga
+     */
     @Override
     public void operate(PaymentSaga paymentSaga) {
+        List<PaymentDetailSaga> paymentDetailSagaList = paymentSaga.getPaymentDetailSagaList();
+        List<PaymentRollbackEvent> paymentRollbackEventList = new ArrayList<>();
+        for (PaymentDetailSaga paymentDetailSaga : paymentDetailSagaList) {
+            PaymentRollbackEvent paymentRollbackEvent
+                    = PaymentDetailSagaMapper.toPaymentRollbackEvent(paymentDetailSaga);
+            paymentRollbackEventList.add(paymentRollbackEvent);
+        }
 
+        PaymentFailEvent paymentFailEvent
+                = PaymentSagaMapper.toPaymentFailEvent(paymentSaga, failReason, paymentRollbackEventList);
+
+        paymentSaga.publishEvent(
+                Topic.PAYMENT_FAIL,
+                paymentSaga.getKey(),
+                paymentFailEvent
+        );
     }
 }
