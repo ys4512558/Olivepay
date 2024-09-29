60212b02dcac78b82017d5a99df838cf91301aaf
ys4512558@naver.com <ys4512558@naver.com>
2024-09-29 20:47:19 +0900

    :sparkles: Feat: 결제 적용 성공/실패 이벤트에 결제 성공에 대한 내역 리스트 추가
    - S11P21A601-499

--- backend/core/src/main/java/kr/co/olivepay/core/transaction/topic/event/payment/result/PaymentApplyFailEvent.java
+++ backend/core/src/main/java/kr/co/olivepay/core/transaction/topic/event/payment/result/PaymentApplyFailEvent.java
@@ -1,17 +1,18 @@
 package kr.co.olivepay.core.transaction.topic.event.payment.result;
 
-import lombok.AllArgsConstructor;
+import kr.co.olivepay.core.payment.dto.res.PaymentApplyHistory;
 import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+
+import java.util.List;
 
 /**
  * 결제 적용 실패 이벤트
  */
 @Builder
 public record PaymentApplyFailEvent(
-    //계좌 잔액 실패 원인 (잔액 부족 등)
-    String failReason
+        //계좌 잔액 실패 원인 (잔액 부족 등)
+        String failReason,
+        List<PaymentApplyHistory> paymentApplyHistoryList
 ) {
 
 }
