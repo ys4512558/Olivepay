2d81357919d3f87aafa2e98ce13c809f297f607b
wnso.kim <wnso.kim@gmail.com>
2024-09-29 21:54:43 +0900

    ✨ Feat: Member: 유저의 비밀번호 검증을 위한 Controller 개발
    - #S11P21A601-501

---
+++ backend/member/src/main/java/kr/co/olivepay/member/controller/UserInfoController.java
@@ -0,0 +1,54 @@
+package kr.co.olivepay.member.controller;
+
+import io.swagger.v3.oas.annotations.Operation;
+import jakarta.validation.Valid;
+import kr.co.olivepay.core.util.CommonUtil;
+import kr.co.olivepay.member.dto.req.UserPasswordCheckReq;
+import kr.co.olivepay.member.dto.res.UserPasswordCheckRes;
+import kr.co.olivepay.member.global.enums.NoneResponse;
+import kr.co.olivepay.member.global.handler.AppException;
+import kr.co.olivepay.member.global.response.Response;
+import kr.co.olivepay.member.global.response.SuccessResponse;
+import kr.co.olivepay.member.service.UserInfoService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import static kr.co.olivepay.member.global.enums.ErrorCode.INTERNAL_SERVER_ERROR;
+
+@Slf4j
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/members/users")
+public class UserInfoController {
+
+    private final UserInfoService userInfoService;
+
+    @PostMapping("/password-check")
+    @Operation(description = "유저 정보 변경 이전, 비밀번호를 입력받아 일치하는지 확인합니다.", summary = "유저 비밀번호 검증")
+    public ResponseEntity<Response<UserPasswordCheckRes>> checkUserPassword(
+            @RequestHeader HttpHeaders headers,
+            @RequestBody @Valid UserPasswordCheckReq request)
+    {
+        Long memberId = getMemberId(headers);
+
+        SuccessResponse<UserPasswordCheckRes> response = userInfoService.checkUserPassword(memberId, request);
+        return Response.success(response);
+    }
+
+    /**
+     * 헤더에서 memberId를 추출하는 메소드 및 에러 핸들링
+     * @param headers
+     * @return
+     */
+    private Long getMemberId(HttpHeaders headers){
+        try {
+            return CommonUtil.getMemberId(headers);
+        } catch (Exception e){
+            log.error("헤더에서 memberId를 추출하는 중 오류가 발생했습니다: {}", e.getMessage());
+            throw new AppException(INTERNAL_SERVER_ERROR);
+        }
+    }
+}
