775ae817729c960d1e567f9a361a78a2c300b5ac
wnso.kim <wnso.kim@gmail.com>
2024-09-29 20:32:34 +0900

    ♻️ Refactor: Member: Javadoc 작성

--- backend/member/src/main/java/kr/co/olivepay/member/service/impl/MemberServiceImpl.java
+++ backend/member/src/main/java/kr/co/olivepay/member/service/impl/MemberServiceImpl.java
@@ -28,6 +28,13 @@ public class MemberServiceImpl implements MemberService {
     private final MemberRepository memberRepository;
     private final MemberMapper memberMapper;
 
+    /**
+     * 회원 정보를 받아 회원 가입하는 메소드<br>
+     * 유저/가맹점주 에서 사용
+     * @param request
+     * @param role
+     * @return
+     */
     @Override
     public Member registerMember(MemberRegisterReq request, Role role){
         // 전화번호 중복 확인
@@ -40,6 +47,12 @@ public class MemberServiceImpl implements MemberService {
         return memberRepository.save(member);
     }
 
+    /**
+     * 전화번호 중복 여부 판단 메소드<br>
+     * 유저/가맹점주 등록 전 확인
+     * @param phoneNumber
+     * @return
+     */
     @Override
     public SuccessResponse<DuplicateRes> checkPhoneNumberDuplicate(final String phoneNumber) {
         Boolean isDuplicated = memberRepository.existsByPhoneNumber(phoneNumber);
@@ -47,6 +60,12 @@ public class MemberServiceImpl implements MemberService {
         return new SuccessResponse<>(CHECK_PHONE_NUMBER_DUPLICATE_SUCCESS, response);
     }
 
+    /**
+     * 회원의 권한을 확인하는 메소드<br>
+     * Gateway에서 사용
+     * @param memberId
+     * @return
+     */
     @Override
     public SuccessResponse<MemberRoleRes> getMemberRole(Long memberId){
         Optional<Member> member = memberRepository.findById(memberId);
@@ -56,6 +75,11 @@ public class MemberServiceImpl implements MemberService {
         return new SuccessResponse<>(GET_MEMBER_ROLE_SUCCESS, response);
     }
 
+    /**
+     * 임시 회원을 일반 회원으로 전화하는 메소드
+     * @param memberId
+     * @return
+     */
     @Override
     public SuccessResponse<NoneResponse> promoteUser(Long memberId){
         // 회원 조회
