eb39c0a09e2946dabb3b1b8cbe43edcf51d9118a
wnso.kim <wnso.kim@gmail.com>
2024-09-29 22:14:19 +0900

    ✨ Feat: Member: 유저의 비밀번호 변경을 위한 Service 개발
    - #S11P21A601-501

--- backend/member/src/main/java/kr/co/olivepay/member/service/UserInfoService.java
+++ backend/member/src/main/java/kr/co/olivepay/member/service/UserInfoService.java
@@ -1,11 +1,18 @@
 package kr.co.olivepay.member.service;
 
+import kr.co.olivepay.member.dto.req.UserPasswordChangeReq;
 import kr.co.olivepay.member.dto.req.UserPasswordCheckReq;
 import kr.co.olivepay.member.dto.res.UserPasswordCheckRes;
+import kr.co.olivepay.member.global.enums.NoneResponse;
 import kr.co.olivepay.member.global.response.SuccessResponse;
 
 public interface UserInfoService {
 
     SuccessResponse<UserPasswordCheckRes> checkUserPassword(
-            Long memberId, UserPasswordCheckReq request);
+            Long memberId, UserPasswordCheckReq request
+    );
+
+    SuccessResponse<NoneResponse> changeUSerPassword(
+            Long memberId, UserPasswordChangeReq request
+    );
 }
--- backend/member/src/main/java/kr/co/olivepay/member/service/impl/UserInfoServiceImpl.java
+++ backend/member/src/main/java/kr/co/olivepay/member/service/impl/UserInfoServiceImpl.java
@@ -1,9 +1,10 @@
 package kr.co.olivepay.member.service.impl;
 
+import kr.co.olivepay.member.dto.req.UserPasswordChangeReq;
 import kr.co.olivepay.member.dto.req.UserPasswordCheckReq;
 import kr.co.olivepay.member.dto.res.UserPasswordCheckRes;
 import kr.co.olivepay.member.entity.Member;
-import kr.co.olivepay.member.global.enums.SuccessCode;
+import kr.co.olivepay.member.global.enums.NoneResponse;
 import kr.co.olivepay.member.global.response.SuccessResponse;
 import kr.co.olivepay.member.repository.MemberRepository;
 import kr.co.olivepay.member.service.UserInfoService;
@@ -12,6 +13,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import static kr.co.olivepay.member.global.enums.SuccessCode.PASSWORD_CHANGE_SUCCESS;
 import static kr.co.olivepay.member.global.enums.SuccessCode.PASSWORD_CHECK_SUCCESS;
 
 @Slf4j
@@ -31,5 +33,16 @@ public class UserInfoServiceImpl implements UserInfoService {
 
         UserPasswordCheckRes response = new UserPasswordCheckRes(isCorrect);
         return new SuccessResponse<>(PASSWORD_CHECK_SUCCESS, response);
+    }
+
+    @Override
+    public SuccessResponse<NoneResponse> changeUSerPassword(
+            Long memberId, UserPasswordChangeReq request)
+    {
+        Member member = memberRepository.getById(memberId);
+        member.updatePassword(passwordEncoder.encode(request.password()));
+
+        memberRepository.save(member);
+        return new SuccessResponse<>(PASSWORD_CHANGE_SUCCESS, NoneResponse.NONE);
     }
 }
