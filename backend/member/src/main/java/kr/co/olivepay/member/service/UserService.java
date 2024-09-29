2a619cf4adc2156709af8a882353c8d408fe2225
wnso.kim <wnso.kim@gmail.com>
2024-09-29 20:09:44 +0900

    ✨ Feat: Member: 금융망 API UserKey 조회를 위한 Service개발
    - #S11P21A601-495

--- backend/member/src/main/java/kr/co/olivepay/member/service/UserService.java
+++ backend/member/src/main/java/kr/co/olivepay/member/service/UserService.java
@@ -1,5 +1,6 @@
 package kr.co.olivepay.member.service;
 
+import kr.co.olivepay.core.member.dto.res.UserKeyRes;
 import kr.co.olivepay.member.dto.req.UserRegisterReq;
 import kr.co.olivepay.member.global.enums.NoneResponse;
 import kr.co.olivepay.member.global.response.SuccessResponse;
@@ -7,4 +8,6 @@ import kr.co.olivepay.member.global.response.SuccessResponse;
 public interface UserService {
 
     SuccessResponse<NoneResponse> registerUser(UserRegisterReq request);
+
+    SuccessResponse<UserKeyRes> getUserKey(Long memberId);
 }
--- backend/member/src/main/java/kr/co/olivepay/member/service/impl/UserServiceImpl.java
+++ backend/member/src/main/java/kr/co/olivepay/member/service/impl/UserServiceImpl.java
@@ -1,15 +1,18 @@
 package kr.co.olivepay.member.service.impl;
 
+import kr.co.olivepay.core.member.dto.res.UserKeyRes;
 import kr.co.olivepay.member.dto.req.UserRegisterReq;
 import kr.co.olivepay.member.entity.Member;
 import kr.co.olivepay.member.entity.User;
 import kr.co.olivepay.member.enums.Role;
 import kr.co.olivepay.member.global.enums.NoneResponse;
 import kr.co.olivepay.member.global.enums.SuccessCode;
+import kr.co.olivepay.member.global.handler.AppException;
 import kr.co.olivepay.member.global.response.SuccessResponse;
 import kr.co.olivepay.member.mapper.UserMapper;
 import kr.co.olivepay.member.openapi.dto.res.member.MemberCreateAndSearchRes;
 import kr.co.olivepay.member.openapi.service.FintechService;
+import kr.co.olivepay.member.repository.MemberRepository;
 import kr.co.olivepay.member.repository.UserRepository;
 import kr.co.olivepay.member.service.MemberService;
 import kr.co.olivepay.member.service.UserService;
@@ -20,7 +23,10 @@ import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.Optional;
+
 import static kr.co.olivepay.member.global.enums.ErrorCode.FINTECH_API_ID_ALREADY_EXISTS;
+import static kr.co.olivepay.member.global.enums.ErrorCode.NOT_FOUND_MEMBER;
 import static kr.co.olivepay.member.global.enums.SuccessCode.*;
 
 @Slf4j
@@ -34,7 +40,13 @@ public class UserServiceImpl implements UserService {
     private final UserRepository userRepository;
     private final UserMapper userMapper;
     private final FintechService fintechService;
+    private final MemberRepository memberRepository;
 
+    /**
+     * 유저 정보를 받아 회원 가입하는 메소드
+     * @param request
+     * @return
+     */
     @Override
     public SuccessResponse<NoneResponse> registerUser(UserRegisterReq request) {
         // 금융망 API 유저 등록
@@ -47,7 +59,28 @@ public class UserServiceImpl implements UserService {
         return new SuccessResponse<>(USER_CREATED, NoneResponse.NONE);
     }
 
+    /**
+     * 멤버ID를 받아 금융망 API UserKey를 반환하는 메소드
+     * @param memberId
+     * @return
+     */
+    @Override
+    public SuccessResponse<UserKeyRes> getUserKey(Long memberId){
+        // member 조회
+        String userKey = userRepository.findUserKeyByMemberId(memberId)
+                        .orElseThrow(() -> new AppException(NOT_FOUND_MEMBER));
 
+        // 반환
+        UserKeyRes response = new UserKeyRes(userKey);
+        return new SuccessResponse<>(GET_USER_KEY_SUCCESS, response);
+    }
+
+
+    /**
+     * 금융망 API에 유저를 등록하는 메소드
+     * @param phoneNumber
+     * @return
+     */
     public String registerFintechMember(String phoneNumber){
         try {
             return fintechService.createMember(phoneNumber + EMAIL_SUFFIX).getUserKey();
@@ -64,6 +97,11 @@ public class UserServiceImpl implements UserService {
     }
 
 
+    /**
+     * 회원 및 임시회원을 등록하는 메소드
+     * @param request
+     * @param UserKey
+     */
     @Transactional
     public void registerUser(UserRegisterReq request, String UserKey){
         // Member 생성
