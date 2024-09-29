46a58200d86dd785e24266da8e1a82869168c54e
wnso.kim <wnso.kim@gmail.com>
2024-09-29 20:09:34 +0900

    ✨ Feat: Member: 금융망 API UserKey 조회를 위한 Repository 개발
    - #S11P21A601-495

--- backend/member/src/main/java/kr/co/olivepay/member/repository/UserRepository.java
+++ backend/member/src/main/java/kr/co/olivepay/member/repository/UserRepository.java
@@ -2,8 +2,14 @@ package kr.co.olivepay.member.repository;
 
 import kr.co.olivepay.member.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
+
+    @Query("SELECT u.userKey FROM User u WHERE u.member.id = :memberId")
+    Optional<String> findUserKeyByMemberId(Long memberId);
 }
