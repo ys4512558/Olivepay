cd65f6b20dfcc0487aa35422779634f8baa2b9d2
ys4512558@naver.com <ys4512558@naver.com>
2024-09-29 23:27:17 +0900

    :sparkles: Feat: 쿠폰 적용 성공/실패 상태 로직 구현
    - S11P21A601-506

--- backend/transaction/src/main/java/kr/co/olivepay/transaction/state/coupon/CouponApplySuccess.java
+++ backend/transaction/src/main/java/kr/co/olivepay/transaction/state/coupon/CouponApplySuccess.java
@@ -1,5 +1,7 @@
 package kr.co.olivepay.transaction.state.coupon;
 
+import kr.co.olivepay.core.transaction.topic.Topic;
+import kr.co.olivepay.core.transaction.topic.event.payment.result.PaymentCompleteEvent;
 import kr.co.olivepay.transaction.PaymentSaga;
 import kr.co.olivepay.transaction.state.PaymentState;
 
@@ -7,6 +9,10 @@ public class CouponApplySuccess implements PaymentState {
 
     @Override
     public void operate(PaymentSaga paymentSaga) {
-
+        paymentSaga.publishEvent(
+                Topic.PAYMENT_COMPLETE,
+                paymentSaga.getKey(),
+                new PaymentCompleteEvent(paymentSaga.getPaymentId())
+        );
     }
 }
