18b34adb4709e207ccd29696df0a707cb284850e
ys4512558@naver.com <ys4512558@naver.com>
2024-09-30 13:29:00 +0900

    :recycle: Refactor: 쿠폰 사용처리 클래스 명 및 필드 명 변경

--- backend/core/src/main/java/kr/co/olivepay/core/transaction/topic/event/coupon/result/CouponUsedSuccessEvent.java
+++ backend/core/src/main/java/kr/co/olivepay/core/transaction/topic/event/coupon/result/CouponUsedSuccessEvent.java
@@ -3,10 +3,10 @@ package kr.co.olivepay.core.transaction.topic.event.coupon.result;
 import lombok.Builder;
 
 /**
- * 쿠폰 적용 성공 이벤트
+ * 쿠폰 사용 처리 성공 이벤트
  */
 @Builder
-public record CouponApplySuccessEvent(
+public record CouponUsedSuccessEvent(
 
 ) {
 
