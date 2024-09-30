6060c586302454b16d66df070277ab202e4a0e26
ys4512558@naver.com <ys4512558@naver.com>
2024-09-30 14:39:44 +0900

    :sparkles: Feat: 쿠폰 잔액 이체 상태 로직 개발
    - S11P21A601-529

---
+++ backend/transaction/src/main/java/kr/co/olivepay/transaction/state/coupon/CouponTransferFail.java
@@ -0,0 +1,51 @@
+package kr.co.olivepay.transaction.state.coupon;
+
+import kr.co.olivepay.core.transaction.topic.Topic;
+import kr.co.olivepay.core.transaction.topic.event.payment.result.PaymentRollbackDetailEvent;
+import kr.co.olivepay.core.transaction.topic.event.payment.result.PaymentRollbackEvent;
+import kr.co.olivepay.transaction.PaymentDetailSaga;
+import kr.co.olivepay.transaction.PaymentSaga;
+import kr.co.olivepay.transaction.mapper.PaymentDetailSagaMapper;
+import kr.co.olivepay.transaction.mapper.PaymentSagaMapper;
+import kr.co.olivepay.transaction.state.PaymentState;
+import lombok.AllArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@AllArgsConstructor
+public class CouponTransferFail implements PaymentState {
+
+    private String failReason;
+
+    @Override
+    public void operate(PaymentSaga paymentSaga) {
+        List<PaymentDetailSaga> paymentDetailSagaList = paymentSaga.getPaymentDetailSagaList();
+        List<PaymentRollbackDetailEvent> paymentRollbackDetailEventList = new ArrayList<>();
+        for (PaymentDetailSaga paymentDetailSaga : paymentDetailSagaList) {
+            PaymentRollbackDetailEvent paymentRollbackDetailEvent
+                    = PaymentDetailSagaMapper.toPaymentRollbackDetailEvent(paymentDetailSaga);
+            paymentRollbackDetailEventList.add(paymentRollbackDetailEvent);
+        }
+
+        publishPaymentRollbackEvent(paymentSaga, paymentRollbackDetailEventList);
+    }
+
+    /**
+     * 결제 적용 실패 -> 결제 롤백 이벤트 발행
+     *
+     * @param paymentSaga
+     * @param paymentRollbackDetailEventList
+     */
+    private void publishPaymentRollbackEvent(PaymentSaga paymentSaga, List<PaymentRollbackDetailEvent> paymentRollbackDetailEventList) {
+        PaymentRollbackEvent paymentRollbackEvent
+                = PaymentSagaMapper.toPaymentRollbackEvent(paymentSaga, failReason, paymentRollbackDetailEventList);
+
+        //payment 서비스로 결제 프로세스 실패 이벤트 발행
+        paymentSaga.publishEvent(
+                Topic.PAYMENT_APPLY_ROLLBACK,
+                paymentSaga.getKey(),
+                paymentRollbackEvent
+        );
+    }
+}
---
+++ backend/transaction/src/main/java/kr/co/olivepay/transaction/state/coupon/CouponTransferSuccess.java
@@ -0,0 +1,30 @@
+package kr.co.olivepay.transaction.state.coupon;
+
+import kr.co.olivepay.core.transaction.topic.Topic;
+import kr.co.olivepay.core.transaction.topic.event.coupon.CouponUsedEvent;
+import kr.co.olivepay.transaction.PaymentSaga;
+import kr.co.olivepay.transaction.state.PaymentState;
+
+public class CouponTransferSuccess implements PaymentState {
+
+    @Override
+    public void operate(PaymentSaga paymentSaga) {
+        publishCouponUsedEvent(paymentSaga);
+    }
+
+    /**
+     * 쿠폰 사용처리 이벤트 발행
+     * @param paymentSaga
+     */
+    private static void publishCouponUsedEvent(PaymentSaga paymentSaga) {
+        CouponUsedEvent couponUsedEvent
+                = CouponUsedEvent.builder()
+                                 .couponUserId(paymentSaga.getCouponUserId())
+                                 .build();
+        paymentSaga.publishEvent(
+                Topic.COUPON_USED,
+                paymentSaga.getKey(),
+                couponUsedEvent
+        );
+    }
+}
