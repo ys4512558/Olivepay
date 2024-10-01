1e21cd1a2a980986fdd71077067cf180c3f7b72b
서정희 <sjhlko@naver.com>
2024-10-01 21:49:15 +0900

    🐛 Fix: @Getter 붙이도록 수정

--- backend/donation/src/main/java/kr/co/olivepay/donation/enums/CouponUnit.java
+++ backend/donation/src/main/java/kr/co/olivepay/donation/enums/CouponUnit.java
@@ -1,8 +1,10 @@
 package kr.co.olivepay.donation.enums;
 
 import lombok.AllArgsConstructor;
+import lombok.Getter;
 
 @AllArgsConstructor
+@Getter
 public enum CouponUnit {
     TWO(2000), FOUR(4000);
     Integer value;
