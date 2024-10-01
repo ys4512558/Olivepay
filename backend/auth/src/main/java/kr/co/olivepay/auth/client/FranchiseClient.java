232809f637f740a0d9f84b893e29e63b2031aaac
wnso.kim <wnso.kim@gmail.com>
2024-10-01 19:49:57 +0900

    ✨ Feat: Auth: Franchise 서비스 사용을 위한 FeignClient 개발
    - #S11P21A601-551

---
+++ backend/auth/src/main/java/kr/co/olivepay/auth/client/FranchiseClient.java
@@ -0,0 +1,16 @@
+package kr.co.olivepay.auth.client;
+
+import kr.co.olivepay.auth.global.response.Response;
+import kr.co.olivepay.core.franchise.dto.res.FranchiseMinimalRes;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@FeignClient(name = "franchise", url = "http://localhost:8104")
+public interface FranchiseClient {
+
+    String MEMBER_ID = "member-id";
+
+    @GetMapping("/api/franchises/id/{memberId}")
+    Response<FranchiseMinimalRes> getFranchiseByMemberId(@PathVariable Long memberId);
+}
