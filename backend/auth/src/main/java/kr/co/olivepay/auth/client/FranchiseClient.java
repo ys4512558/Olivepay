3b617370dc545577ef587300519028c8a54dda68
wnso.kim <wnso.kim@gmail.com>
2024-10-01 19:53:38 +0900

    🚑️ !HOTFIX: Auth: FeignClient 호출 배포환경에 맞도록 수정
    - #S11P21A601-551

--- backend/auth/src/main/java/kr/co/olivepay/auth/client/FranchiseClient.java
+++ backend/auth/src/main/java/kr/co/olivepay/auth/client/FranchiseClient.java
@@ -6,7 +6,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "franchise", url = "http://localhost:8104")
+@FeignClient(name = "franchise")
 public interface FranchiseClient {
 
     String MEMBER_ID = "member-id";
