d7b1600d926106ec2a55efa9539c2d88250acc5e
ys4512558@naver.com <ys4512558@naver.com>
2024-10-02 17:10:33 +0900

    :sparkles: Feat: EnableKafka추가

--- backend/donation/src/main/java/kr/co/olivepay/donation/DonationApplication.java
+++ backend/donation/src/main/java/kr/co/olivepay/donation/DonationApplication.java
@@ -3,7 +3,9 @@ package kr.co.olivepay.donation;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.kafka.annotation.EnableKafka;
 
+@EnableKafka
 @SpringBootApplication
 @EnableDiscoveryClient
 public class DonationApplication {
