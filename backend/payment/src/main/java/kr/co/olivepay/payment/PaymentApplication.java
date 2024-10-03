4d4c61d522105bac2b31fbbf09b69682b2a94d6a
김진이 <jinniek48@gmail.com>
2024-10-03 15:30:06 +0900

    :sparkles: Feat: 유저 결제 내역 조회 api에 필요한 외부 서비스 연결
    -#S11P21A601-470

--- backend/payment/src/main/java/kr/co/olivepay/payment/PaymentApplication.java
+++ backend/payment/src/main/java/kr/co/olivepay/payment/PaymentApplication.java
@@ -3,9 +3,13 @@ package kr.co.olivepay.payment;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.kafka.annotation.EnableKafka;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @EnableKafka
 @SpringBootApplication
+@EnableDiscoveryClient
+@EnableFeignClients(basePackages = "kr.co.olivepay.payment.client")
 public class PaymentApplication {
 
 	public static void main(String[] args) {
