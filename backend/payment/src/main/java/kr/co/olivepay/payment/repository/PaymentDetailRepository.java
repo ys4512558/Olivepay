0175445d393fc109a4e3f42db609542a4881cc08
김진이 <jinniek48@gmail.com>
2024-10-05 12:18:58 +0900

    ♻️ Refactor: entity 업데이트 컨벤션 반영

--- backend/payment/src/main/java/kr/co/olivepay/payment/repository/PaymentDetailRepository.java
+++ backend/payment/src/main/java/kr/co/olivepay/payment/repository/PaymentDetailRepository.java
@@ -17,9 +17,4 @@ public interface PaymentDetailRepository extends JpaRepository<PaymentDetail, Lo
 
 	List<PaymentDetail> findAllByPaymentId(Long paymentId);
 
-	@Modifying
-	@Transactional
-	@Query("UPDATE PaymentDetail pd SET pd.paymentDetailState = :paymentState WHERE pd.id = :id")
-	void updatePaymentDetailState(Long id, PaymentState paymentState);
-
 }
