8dd9b23f48cbce42a9cef33898fb87a52a6816fd
서정희 <sjhlko@naver.com>
2024-10-07 09:49:55 +0900

    ✨ Feat: 쿠폰 사용 kafka 관련 로직 추가

---
+++ backend/donation/src/main/java/kr/co/olivepay/donation/service/impl/DonationEventServiceImpl.java
@@ -0,0 +1,36 @@
+package kr.co.olivepay.donation.service.impl;
+
+import kr.co.olivepay.donation.dto.res.CouponUsedStateRes;
+import kr.co.olivepay.core.transaction.topic.event.coupon.CouponUsedEvent;
+import kr.co.olivepay.donation.entity.CouponUser;
+import kr.co.olivepay.donation.repository.CouponUserRepository;
+import kr.co.olivepay.donation.service.DonationEventService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.Objects;
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class DonationEventServiceImpl implements DonationEventService {
+    private final CouponUserRepository couponUserRepository;
+    private final String FAIL_REASON = "쿠폰이 유효하지 않습니다.";
+    @Override
+    public CouponUsedStateRes useCoupon(CouponUsedEvent event) {
+        Boolean isSuccess = false;
+        Optional<CouponUser> couponUser = couponUserRepository.findCouponUserById(event.couponUserId());
+        if (couponUser.isPresent()) {
+            if(!couponUser.get().getIsUsed() && Objects.equals(couponUser.get().getMemberId(), event.memberId())){
+                isSuccess = true;
+                CouponUser existCouponuser = couponUser.get();
+                existCouponuser.updateIsUsed(true);
+                couponUserRepository.save(existCouponuser);
+            }
+        }
+        return CouponUsedStateRes.builder()
+                                 .isSuccess(isSuccess)
+                                 .failReason(isSuccess ? null : FAIL_REASON)
+                                 .build();
+    }
+}
