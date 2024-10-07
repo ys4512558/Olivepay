fd824970ab3d9b1223ec86217c71d5ef3ba29ce3
iltae94 <iltae94@gmail.com>
2024-10-07 16:27:26 +0900

    :sparkles: Feat: 쿠폰 획득 API 비동기 처리

--- frontend/src/api/couponApi.ts
+++ frontend/src/api/couponApi.ts
@@ -24,9 +24,9 @@ export const acquireCoupon = async (
   couponUnit: number,
   franchiseId: number,
 ) => {
-  const response = Axios.post(`${prefix}/my`, {
+  const response = await Axios.post(`${prefix}/my`, {
     couponUnit: couponUnit,
     franchiseId: franchiseId,
   });
-  return response;
+  return response.data;
 };
