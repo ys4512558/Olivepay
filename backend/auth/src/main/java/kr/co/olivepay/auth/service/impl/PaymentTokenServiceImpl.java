1a7bb8dac24fa4ac7dce1d291817b0a139043c8d
wnso.kim <wnso.kim@gmail.com>
2024-10-08 00:16:12 +0900

    ✨ Feat: Auth: 결제 토큰 발급 API를 위한 Service 및 상태코드 개발
    - #S11P21A601-603

---
+++ backend/auth/src/main/java/kr/co/olivepay/auth/service/impl/PaymentTokenServiceImpl.java
@@ -0,0 +1,38 @@
+package kr.co.olivepay.auth.service.impl;
+
+import kr.co.olivepay.auth.dto.res.PaymentTokenRes;
+import kr.co.olivepay.auth.enums.Role;
+import kr.co.olivepay.auth.global.response.SuccessResponse;
+import kr.co.olivepay.auth.global.utils.TokenGenerator;
+import kr.co.olivepay.auth.service.PaymentTokenService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+import java.time.Duration;
+import java.util.HashMap;
+
+import static kr.co.olivepay.auth.global.enums.SuccessCode.GET_PAYMENT_TOKEN_SUCCESS;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class PaymentTokenServiceImpl implements PaymentTokenService {
+
+    private final TokenGenerator tokenGenerator;
+    private final Role USER = Role.USER;
+    private final Duration PAYMENT_TOKEN_EXPIRATION = Duration.ofMinutes(1);
+
+
+    @Override
+    public SuccessResponse<PaymentTokenRes> getPaymentToken(Long memberId) {
+        String paymentToken =
+                tokenGenerator.generateToken(new HashMap<>(), memberId, USER, PAYMENT_TOKEN_EXPIRATION);
+
+        PaymentTokenRes response = PaymentTokenRes.builder()
+                                                  .paymentToken(paymentToken)
+                                                  .build();
+
+        return new SuccessResponse<>(GET_PAYMENT_TOKEN_SUCCESS, response);
+    }
+}
