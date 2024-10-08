4469501b8ed95c146bbb71e6ff9cfd1786543ff1
ys4512558@naver.com <ys4512558@naver.com>
2024-10-08 13:58:50 +0900

    :sparkles: Feat: 결제 프로세스 성공 소켓 데이터 전송 로직 추가

--- backend/payment/src/main/java/kr/co/olivepay/payment/transaction/listener/PaymentCompleteEventListener.java
+++ backend/payment/src/main/java/kr/co/olivepay/payment/transaction/listener/PaymentCompleteEventListener.java
@@ -4,12 +4,16 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import kr.co.olivepay.core.transaction.topic.Topic;
 import kr.co.olivepay.core.transaction.topic.event.payment.result.PaymentCompleteEvent;
+import kr.co.olivepay.payment.global.enums.NoneResponse;
 import kr.co.olivepay.payment.global.properties.KafkaProperties;
+import kr.co.olivepay.payment.global.response.Response;
 import kr.co.olivepay.payment.service.PaymentEventService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Component;
 
 @Slf4j
@@ -17,8 +21,11 @@ import org.springframework.stereotype.Component;
 @RequiredArgsConstructor
 public class PaymentCompleteEventListener implements KafkaEventListener {
 
+    @Value("${config.socket.TOPIC_PREFIX}")
+    private String TOPIC_PREFIX;
     private final ObjectMapper objectMapper;
     private final PaymentEventService paymentEventService;
+    private final SimpMessagingTemplate simpMessagingTemplate;
 
     @Override
     @KafkaListener(topics = Topic.PAYMENT_COMPLETE, groupId = KafkaProperties.KAFKA_GROUP_ID_CONFIG)
@@ -34,6 +41,16 @@ public class PaymentCompleteEventListener implements KafkaEventListener {
             paymentEventService.paymentComplete(paymentCompleteEvent);
             log.info("PaymentCompleteEvent : {}", paymentCompleteEvent);
             log.info("결제 프로세스 종료 : [결제 성공]");
+            String topic = TOPIC_PREFIX + "/" + key;
+
+            Response<NoneResponse> success
+             = new Response<>("SUCCESS", "PAYMENT_SUCCESS", "결제에 성공하였습니다.", NoneResponse.NONE);
+
+            String payload
+                    = objectMapper.writeValueAsString(success);
+
+            simpMessagingTemplate.convertAndSend(topic, payload);
+            log.info("결제 프로세스 종료 : [결제 성공 전달 완료]");
         } catch (JsonProcessingException e) {
             throw new RuntimeException(e);
         }
