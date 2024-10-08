e505d7959bfede3b7d634d7928e657b5c4c7a71c
ys4512558@naver.com <ys4512558@naver.com>
2024-10-08 13:06:45 +0900

    :sparkles: Feat: 토큰 유틸 관련 유틸 메서드 추가
    - 토큰 만료 여부 확인 메서드

--- backend/gateway/src/main/java/kr/co/olivepay/gateway/global/util/TokenUtils.java
+++ backend/gateway/src/main/java/kr/co/olivepay/gateway/global/util/TokenUtils.java
@@ -1,13 +1,16 @@
 package kr.co.olivepay.gateway.global.util;
 
-import kr.co.olivepay.gateway.global.config.JwtConfig;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
+import kr.co.olivepay.gateway.global.config.JwtConfig;
 import kr.co.olivepay.gateway.global.handler.AppException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import java.util.Date;
 import java.util.function.Function;
 
 import static kr.co.olivepay.gateway.global.enums.ErrorCode.TOKEN_INVALID;
@@ -18,9 +21,11 @@ import static kr.co.olivepay.gateway.global.enums.ErrorCode.TOKEN_INVALID;
 public class TokenUtils {
 
     private final JwtConfig jwtConfig;
+    private static final ZoneId ZONE_ID = ZoneId.of("Asia/Seoul");
 
     /**
      * 토큰 유효성 검사
+     *
      * @param token
      * @return
      */
@@ -37,18 +42,40 @@ public class TokenUtils {
     }
 
     public Long extractMemberId(String token) {
-        return token==null? null:
+        return token == null ? null :
                 Long.parseLong(extractClaim(token, Claims::getSubject));
     }
 
     public String extractRole(String token) {
-        return token==null? null:
+        return token == null ? null :
                 extractClaim(token, claims -> claims.get("role", String.class));
     }
 
     private <T> T extractClaim(String token, Function<Claims, T> claimsResolvers) {
         final Claims claims = extractAllClaims(token);
         return claimsResolvers.apply(claims);
+    }
+
+    /**
+     * 현재 시간 기준 토큰이 만료되었는지 확인
+     *
+     * @param token
+     * @return
+     */
+    public Boolean isTokenExpired(String token) {
+        Claims claims = extractAllClaims(token);
+        Date expiration = claims.getExpiration();
+        log.info("만료 시간 : [{}]", expiration);
+
+        // Date -> ZonedDateTime 변환 시 시간대 맞춰주기
+        ZonedDateTime expirationTime = ZonedDateTime.ofInstant(expiration.toInstant(), ZONE_ID);
+        ZonedDateTime now = ZonedDateTime.now(ZONE_ID);
+        log.info("현재 시간 : [{}]", now);
+
+        // 만료 여부 비교
+        log.info("만료 여부 [{}]", expirationTime.isBefore(now));
+        return expirationTime.isBefore(now);
+
     }
 
     private Claims extractAllClaims(String token) {
@@ -58,7 +85,7 @@ public class TokenUtils {
                        .build()
                        .parseSignedClaims(token)
                        .getPayload();
-        } catch (Exception e){
+        } catch (Exception e) {
             log.error("JWT 토큰 만료");
             throw new AppException(TOKEN_INVALID);
         }
