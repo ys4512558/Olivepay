10243c4b8fadb3459b4ea93c94193f79dd07db81
iltae94 <iltae94@gmail.com>
2024-10-08 19:02:39 +0900

    :bug: Fix: 빌드 에러 수정

--- frontend/src/atoms/franchiseAtom.ts
+++ frontend/src/atoms/franchiseAtom.ts
@@ -17,69 +17,7 @@ export const franchiseListAtom = atom<restaurants>([]);
 
 export const franchiseDetailAtom = atom<franchise | null>(null);
 
-export const franchiseIncomeAtom = atom<paymentList>([
-  {
-    paymentId: 1,
-    amount: 13500,
-    createdAt: '2024년 9월 10일',
-    details: [
-      {
-        type: 'card',
-        name: '꿈나무카드',
-        amount: 9000,
-      },
-      {
-        type: 'coupon',
-        name: '4000',
-        amount: 4000,
-      },
-      {
-        type: 'card',
-        name: '신한4582',
-        amount: 500,
-      },
-    ],
-  },
-  {
-    paymentId: 2,
-    amount: 16000,
-    createdAt: '2024년 9월 10일',
-    details: [
-      {
-        type: 'card',
-        name: '꿈나무카드',
-        amount: 9000,
-      },
-      {
-        type: 'coupon',
-        name: '4000',
-        amount: 4000,
-      },
-      {
-        type: 'card',
-        name: '신한4582',
-        amount: 3000,
-      },
-    ],
-  },
-  {
-    paymentId: 5,
-    amount: 10500,
-    createdAt: '2024년 9월 12일',
-    details: [
-      {
-        type: 'card',
-        name: '꿈나무카드',
-        amount: 9000,
-      },
-      {
-        type: 'coupon',
-        name: '2000',
-        amount: 1500,
-      },
-    ],
-  },
-]);
+export const franchiseIncomeAtom = atom<paymentList>([]);
 
 export const franchiseCouponAtom = atom<coupon>({
   coupon2: 12,
