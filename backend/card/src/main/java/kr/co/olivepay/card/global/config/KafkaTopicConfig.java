d0a7185e086dae27f0de7967c30e369c7a5e2416
seungcheol <ys4512558@naver.com>
2024-10-10 00:36:35 +0900

    :sparkles: Feat: 카프카 토픽 설정 추가

---
+++ backend/card/src/main/java/kr/co/olivepay/card/global/config/KafkaTopicConfig.java
@@ -0,0 +1,50 @@
+package kr.co.olivepay.card.global.config;
+
+import kr.co.olivepay.card.global.properties.KafkaProperties;
+import kr.co.olivepay.core.transaction.topic.Topic;
+import lombok.RequiredArgsConstructor;
+import org.apache.kafka.clients.admin.NewTopic;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.config.TopicBuilder;
+import org.springframework.kafka.core.KafkaAdmin;
+
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Configuration
+@RequiredArgsConstructor
+public class KafkaTopicConfig {
+
+    private final KafkaProperties kafkaProperties;
+    private final String MIN_INSYNC_REPLICAS = "min.insync.replicas";
+    private final String BOOTSTRAP_SERVERS = "bootstrap.servers";
+
+    @Bean
+    public KafkaAdmin kafkaAdmin() {
+        List<String> kafkaServers = kafkaProperties.getKAFKA_SERVERS();
+        String bootstrapServer = String.join(",", kafkaServers);
+        Map<String, Object> config = new HashMap<>();
+        config.put(BOOTSTRAP_SERVERS, bootstrapServer);
+        return new KafkaAdmin(config);
+    }
+    @Bean
+    public NewTopic createAccountBalanceCheckSuccess() {
+        return TopicBuilder.name(Topic.ACCOUNT_BALANCE_CHECK_SUCCESS)
+                           .partitions(1)
+                           .replicas(2)
+                           .configs(Collections.singletonMap(MIN_INSYNC_REPLICAS, "2"))
+                           .build();
+    }
+
+    @Bean
+    public NewTopic createAccountBalanceCheckFail() {
+        return TopicBuilder.name(Topic.ACCOUNT_BALANCE_CHECK_FAIL)
+                           .partitions(1)
+                           .replicas(2)
+                           .configs(Collections.singletonMap(MIN_INSYNC_REPLICAS, "2"))
+                           .build();
+    }
+}
