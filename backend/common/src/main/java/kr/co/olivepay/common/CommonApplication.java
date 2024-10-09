791837ecc7ccf98dcfa4a4dbf39ee1b43a3c640b
서정희 <sjhlko@naver.com>
2024-10-10 03:24:29 +0900

    ♻️ Refactor : 이메일, 전화번호 인증번호 발송 관련 비동기 처리 추가

--- backend/common/src/main/java/kr/co/olivepay/common/CommonApplication.java
+++ backend/common/src/main/java/kr/co/olivepay/common/CommonApplication.java
@@ -3,7 +3,9 @@ package kr.co.olivepay.common;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.scheduling.annotation.EnableAsync;
 
+@EnableAsync
 @SpringBootApplication
 @EnableDiscoveryClient
 public class CommonApplication {
