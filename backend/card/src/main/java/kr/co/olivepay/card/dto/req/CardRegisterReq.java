8e9c0792b38fb35165b1a6e1f1bb6287307bfb7c
ys4512558@naver.com <ys4512558@naver.com>
2024-10-10 14:17:57 +0900

    :sparkles: Feat: 카드 등록 유효성 검증 로직 추가

--- backend/card/src/main/java/kr/co/olivepay/card/dto/req/CardRegisterReq.java
+++ backend/card/src/main/java/kr/co/olivepay/card/dto/req/CardRegisterReq.java
@@ -1,6 +1,7 @@
 package kr.co.olivepay.card.dto.req;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import lombok.Builder;
 import org.hibernate.validator.constraints.Length;
 
@@ -11,9 +12,11 @@ public record CardRegisterReq(
         String realCardNumber,
         @NotBlank(message = "유효기간(년)은 공백일 수 없습니다.")
         @Length(min = 2, max = 2, message = "유효기간(년)은 2자리 입니다.")
+        @Pattern(regexp = "^[0-9]{2}$", message = "2자리 숫자만 입력 가능합니다.")
         String expirationYear,
         @NotBlank(message = "유효기간(월)은 공백일 수 없습니다.")
         @Length(min = 2, max = 2, message = "유효기간(월)은 2자리 입니다.")
+        @Pattern(regexp = "^(0[1-9]|1[0-2])$", message = "유효기간(월)은 01에서 12 사이의 두 자리 숫자여야 합니다.")
         String expirationMonth,
         @NotBlank(message = "CVC는 공백일 수 없습니다.")
         @Length(min = 3, max = 3, message = "CVC는 3자리 입니다.")
